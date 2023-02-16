9e4d93269c8a6b0b0f43b35f95478fe62a6ea09c
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-02-16 08:06:53 +0100

    Added services into class

---
+++ classes/ServicesSlide.class.php
@@ -0,0 +1,18 @@
+<?php
+class ServicesSlide extends Slider{
+    // Fetch data from MySQL using PDO - PHP Data Object	
+	public function renderSlider(){
+        $sql = "SELECT * FROM mirnesglamocic_cert.services";
+        $stmt = $this->__connect()->query($sql);
+
+        while($row = $stmt->fetch()) {
+        	$service_img = $row['service_img'];
+			$service_alt = $row['service_alt'];
+            $title = $row['title'];
+            $description = $row['description'];
+            echo "<service class='services'>
+                    <img src=\"./services/$service_img\" alt=\"$service_alt\" class='usluge'><h4>$title</h4><p>$description</p>
+                  </service>";
+            }
+        }
+    }
