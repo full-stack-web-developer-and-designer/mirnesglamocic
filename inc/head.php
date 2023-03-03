ece1fdd2e4a12e49385af77c071ad23fe68418ea
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-03 14:21:16 +0100

    included picturefill.js

--- inc/head.php
+++ inc/head.php
@@ -1,10 +1,10 @@
 <head id="head">
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3.0">
+<meta name="viewport" content="width=device-width">
 <!-- meta tags -->
 <meta name="robots" content="index,follow">
-<meta name="description" content="<?php echo $meta['description']; ?>" />
-<meta name="keywords" content="<?php echo $meta['keywords']; ?>" />
+<meta name="description" content="<?php echo $meta['description']; ?>"/>
+<meta name="keywords" content="<?php echo $meta['keywords']; ?>"/>
 <meta name="theme-color" content="#e84e1b">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="author" content="Mirnes Glamočić">
@@ -38,14 +38,20 @@
 <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-iphone-retina-120x120.png">
 <link rel="apple-touch-icon" sizes="144x144" href="images/apple-touch-icon-ipad-retina-152x152.png">
 <link rel="canonical" href="https://mirnesglamocic.com/" />
+<!-- include picturefill -->
+<script>
+    // Picture element HTML5 shiv
+    document.createElement( "picture" );
+  </script>
+  <script src="./JS/picturefill.min.js" async></script>
 <!-- include fontawesome --> 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 <script src="https://kit.fontawesome.com/c3fb2ab6eb.js" crossorigin="anonymous"></script>
 <!-- lightslider CSS-->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/css/lightslider.min.css" rel="stylesheet"/>
 <!-- Custom CSS -->
-<link rel="stylesheet" href="./CSS/style.css" />
-<link rel="stylesheet" href="./CSS/responsive.css" />
+<link rel="stylesheet" href="./CSS/style.css" media="all"/>
+<link rel="stylesheet" href="./CSS/responsive.css" media="all"/>
 <!-- jQuery -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script>
@@ -55,23 +61,27 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/js/lightslider.min.js"></script>
 
-<!-- jQuery Modal -->
-<script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
 <!--jQuery validate plugin -->
 <script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
 <!-- lightbox css -->
 <link rel="stylesheet" type="text/css" href="/lightbox2/src/css/lightbox.css">
 <noscript><p class="errorJS">To display the page correctly, please enable JavaScript in your browser!</p></noscript>
 <title><?php echo $title; ?></title>
-<script defer>
+<!-- picturefill -->
+<script>
+    // Picture element HTML5 shiv
+    document.createElement( "picture" );
+  </script>
+  <script src="./JS/picturefill.min.js" async></script>
+<!-- responsive navigation -->
+<script>
 var navigation=$.noConflict();
 	navigation(document).ready(function($) {
 		var respmenu = $('#respmenu');
 		var menu = $('#nav>ul');
 		$(respmenu).on('click', function(e){
 			e.preventDefault();
-			menu.slideToggle();
+			menu.toggleClass("open");
 		});
 		$(window).resize(function(){
 			var sirina = $(window).width();
@@ -98,4 +108,5 @@ window.onorientationchange = function() {
 
 };
 </script>
+
 </head>
