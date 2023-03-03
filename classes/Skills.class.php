f461a8b0c9135aebffc9c2802e849dcd0a39c294
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-03 14:23:07 +0100

    Created responsive images with picturefill

--- classes/Skills.class.php
+++ classes/Skills.class.php
@@ -7,10 +7,12 @@ class Skills extends ConnectSlider{
        
         while($row = $stmt->fetch()) {
             $help_tip=$row['help_tip'];
-			$alt=$row['alt'];
             $img=$row['img'];
+            $picturefill_img=$row['picturefill_img'];
+			$alt=$row['alt'];
 
-            echo "<div class='tooltip'><div class='help_tip'><p>$help_tip</p></div></div><img src='./images/$img' alt='$alt' class='skill'>";
+
+            echo "<div class='tooltip'><div class='help_tip'><p>$help_tip</p></div></div><img src=\"./images/$img\" srcset=\"./images/$picturefill_img\" alt='$alt' class='skill'>";
             }
         }
     }
