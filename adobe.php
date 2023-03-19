646d901d6ce47d177136efa8d85bf2cd983228c0
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-20 07:06:52 +0900

    Added aria labels for arrows and improved meta description and footer

--- adobe.php
+++ adobe.php
@@ -6,14 +6,14 @@ require_once 'core/init.php';
 <head>
 <?php 
 	$title="Mirnes Glamočić | Certificates about Adobe CC Applications";
-    //Check length of title for SEO (50-70 characters) - 42
-    //echo "Length of title is " . strlen($title) . "characters long." . " ";
+    //Check length of title for SEO (50-70 characters) - 60
+    echo "Length of title is " . strlen($title) . "characters long." . " ";
     $og_title="Mirnes Glamočić | Certificates about Adobe CC Applications";
 	$website_url="https://mirnesglamocic.com";
-    $meta['description']="Mirnes Glamočić is an experienced Web Developer and Designer with several years of experience in the web development and design industry ";
-    //Check length of description for SEO (140-180 characters) - 139
-    //echo "Length of description is " . strlen($meta['description']) . "characters long.";
-	$meta['keywords']="web development, web developer, front-end development, frontend development, front-end developer, frontend developer, backend-development, backend development, back-end developer, backend developer, database development database developer, web design, web designer";
+    $meta['description']="Mirnes Glamočić is an experienced Web Designer with several years of experience in the web design industry using various Adobe Tools.";
+    //Check length of description for SEO (140-180 characters) - 135
+    echo "Length of description is " . strlen($meta['description']) . "characters long.";
+	$meta['keywords']="web design, web designer, adobe creative cloud, adobe cc, photoshop, illustrator, lightroom, bridge, animate, dreamweaver";
 	$og_image="./images/MirnesGlamocic.jpg";
 	$image_alt="Mirnes Glamočić";
 	include './inc/head.php';
@@ -28,10 +28,10 @@ include 'inc/header.php';
 	
 	<section class="slider-container">
 	<div class="slideControls">
-            <a class="slidePrev arrow">
+            <a class="slidePrev arrow" href="#" aria-label="See the previous certificate">
               <i class="fa fa-angle-left"></i>
              </a>
-            <a class="slideNext arrow">
+            <a class="slideNext arrow" href="#" aria-label="See the following certificate">
               <i class="fa fa-angle-right"></i>
             </a>
       </div><!--end .slideControls -->
@@ -46,10 +46,10 @@ include 'inc/header.php';
 
 	<section class="slider-container">
 	<div class="slideControls">
-            <a class="slidePrev arrow">
+            <a class="slidePrev arrow" href="#" aria-label="See the previous certificate">
               <i class="fa fa-angle-left"></i>
              </a>
-            <a class="slideNext arrow">
+            <a class="slideNext arrow" href="#" aria-label="See the following certificate">
               <i class="fa fa-angle-right"></i>
             </a>
       </div><!--end .slideControls -->
@@ -64,10 +64,10 @@ include 'inc/header.php';
 
 	<section class="slider-container">
 	<div class="slideControls">
-            <a class="slidePrev arrow">
+            <a class="slidePrev arrow" href="#" aria-label="See the previous certificate">
               <i class="fa fa-angle-left"></i>
              </a>
-            <a class="slideNext arrow">
+            <a class="slideNext arrow" href="#" aria-label="See the following certificate">
               <i class="fa fa-angle-right"></i>
             </a>
       </div><!--end .slideControls -->
@@ -81,12 +81,7 @@ include 'inc/header.php';
 	</section><!--end .slider-container-->
 
 </div><!--end .wrapper-->
-<a class="scroll-top" rel="nofollow"><i class="fa fa-arrow-up"></i></a>
-<script src="lightbox2/src/js/lightbox.js"></script>
-<script src='./JS/slider.js'></script>
 <?php
 	include_once './inc/footer.php';
 	//end #footer
-?>  
-</body>
-</html>
+?>
