6ca834bb2db38591d4e2b388aca766a7ad382afd
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-20 03:43:15 +0900

    Added progressive imgs

--- classes/JavaScriptSlide.class.php
+++ classes/JavaScriptSlide.class.php
@@ -2,7 +2,7 @@
 class JavaScriptSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='JavaScript' AND class='LinkedIn' OR skill='JavaScript WebDesign' OR skill='JavaScript WebDesigners' OR skill='JavaScript JSON' ORDER BY rand();";
+        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='JavaScript' AND class='LinkedIn preview' OR skill='JavaScript WebDesign' OR skill='JavaScript WebDesigners' OR skill='JavaScript JSON' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
         while($row = $stmt->fetch()) {
@@ -10,8 +10,8 @@ class JavaScriptSlide extends Slider{
 			$alt = $row['alt'];
             $class = $row['class'];
             echo "<li class='item-a slide'>
-            <article>
-            <a href='./cert/BIG/$picture' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\"></a>
+            <article class='cert'>
+            <a href='./cert/BIG/$picture' data-href='./cert/SMALL/$picture' class='progressive replace' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\" loading=\"lazy\"></a>
             </article></li>";
             }
         }
