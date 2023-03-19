09f9d890c6497591205c5c6bb539aca64b881535
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-20 06:53:22 +0900

    Checkked length of titlče and description

--- seo.php
+++ seo.php
@@ -7,12 +7,12 @@ require_once 'core/init.php';
 <?php 
 	$title="Mirnes Glamočić | SEO Certificates";
     //Check length of title for SEO (50-70 characters) - 36
-    echo "Length of title is " . strlen($title) . "characters long." . " ";
+    //echo "Length of title is " . strlen($title) . "characters long." . " ";
     $og_title="Mirnes Glamočić | SEO Certificates";
 	$website_url="https://mirnesglamocic.com";
     $meta['description']="Mirnes Glamočić is an experienced SEO Analist with several years of experience in the web development and design.";
     //Check length of description for SEO (140-180 characters) - 115
-    echo "Length of description is " . strlen($meta['description']) . "characters long.";
+    //echo "Length of description is " . strlen($meta['description']) . "characters long.";
 	$meta['keywords']="web development, Search Engine Optimization, SEO";
 	$og_image="./images/MirnesGlamocic.jpg";
 	$image_alt="Mirnes Glamočić";
