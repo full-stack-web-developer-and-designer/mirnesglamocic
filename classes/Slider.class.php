697ae415625de53cb7bc4cdb98f88ec5e9b88d9a
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-20 03:51:36 +0900

    Added progressive imgs and loading lazy

--- classes/Slider.class.php
+++ classes/Slider.class.php
@@ -6,8 +6,10 @@ class Slider extends ConnectSlider {
 	$stmt = $this->__connect()->query($sql);
 	
 	while($row = $stmt->fetch()) {
-		echo "<li class='item-a'><article class='product'>
-		<a href=\"cert/BIG/'.$picture.'\" data-lightbox='image-group'><img src=\"cert/SMALL/'.$picture.'\" alt=\"'.$alt.'\" class=\"'.#class.'\"></a></article></li>";
+		echo "<li class='item-a slide'>
+		<article class='cert'>
+		<a href='./cert/BIG/$picture' data-href='./cert/SMALL/$picture' class='progressive replace' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\" loading=\"lazy\"></a>
+		</article></li>";
 		}
 	}
 }
