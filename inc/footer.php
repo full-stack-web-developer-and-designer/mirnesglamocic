66da2dfc69171e7604a5239e2a66b3a137f726ac
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-20 03:29:13 +0900

    Added script tags into footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,14 +1,23 @@
+<a class="scroll-top" rel="nofollow"><i class="fa fa-arrow-up"></i></a>
+<?php $url = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>
 <footer id="footer">
 <?php
 function copyright($startYear) {
 	$currentYear = date('Y');
 	if ($startYear < $currentYear) {
 		$currentYear = date('y');
-		return "<p><small>&copy Copyright $startYear &ndash; $currentYear" . " | mirnesglamocic.com<span class='footer_comma'>,</span></small></p>";
+		return "<p class='no_print'><small>&copy Copyright $startYear &ndash; $currentYear" . " | mirnesglamocic.com<span class='footer_comma'>,</span></small></p>";
 	} else {
-		return "<p><small>&copy Copyright $startYear " . " | Mirnes Glamočić<span class='footer_comma'>,</span></small></p>";
+		return "<p class='no_print'><small>&copy Copyright $startYear " . " | Mirnes Glamočić<span class='footer_comma'>,</span></small></p>";
 	}
 }
-echo copyright(2022);
+echo copyright(2023);
 ?>
 </footer>
+<?php echo "<p class=\"barcode\">This page was printed from: ".$url;?></p>
+
+<script src="lightbox2/src/js/lightbox.js"></script>
+<script src='./JS/slider.js'></script>
+<script src="https://cdn.jsdelivr.net/npm/progressive-image.js/dist/progressive-image.js" async></script>
+</body>
+</html>
