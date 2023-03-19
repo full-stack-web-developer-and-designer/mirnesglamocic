b07211f88e592743721b75a431e9c86f528908f3
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-20 03:42:24 +0900

    Added progressive imgs

--- classes/BootstrapSlide.class.php
+++ classes/BootstrapSlide.class.php
@@ -5,14 +5,13 @@ class BootstrapSlide extends Slider{
         $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='Bootstrap' OR skill='Bootstrap WebDesign' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
-
         while($row = $stmt->fetch()) {
         	$picture = $row['picture'];
 			$alt = $row['alt'];
             $class = $row['class'];
             echo "<li class='item-a slide'>
-            <article>
-            <a href='./cert/BIG/$picture' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\"></a>
+            <article class='cert'>
+            <a href='./cert/BIG/$picture' data-href='./cert/SMALL/$picture' class='progressive replace' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\" loading=\"lazy\"></a>
             </article></li>";
             }
         }
