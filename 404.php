8e7be6dc67a784dadfa7f8b0c9ca57ed83ffb108
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-20 06:43:35 +0900

    Created 404 error page

--- 404.php
+++ 404.php
@@ -2,15 +2,15 @@
 	require_once 'core/init.php';
 ?>
 <!DOCTYPE html>
-<html lang="bs-BA">
+<html lang="en">
 <?php	
 	$title="Error 404!";
 	$og_title="Error 404!";
-	$website_url="www.visit-neum.com/";
+	$website_url="www.mirnesglamocic.com/";
 	$meta['description']="Error 404!";
 	$meta['keywords']="";
-	$og_image="images/error.404.png";
-	$image_alt="Gre&scaron;ka!";
+	$og_image="./images/error.404.png";
+	$image_alt="Error 404!";
 	include 'inc/head.php';	
 ?>
 <style>
@@ -28,7 +28,6 @@
 ?>
 </header><!-- end #header -->
 <div id="main"><p><b><i>"The requested page does not exist!<br>Please check if you entered the address correctly!"</i></b></p></div>
-<aside id="sidebar"></aside><!-- end #sidebar --> 
 <?php
 	include 'inc/footer.php';
 	//end #footer
