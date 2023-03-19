cb9cb4dfe4095fa09b9890f45633ade8bd26dc3e
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-20 03:44:22 +0900

    Added progressive imgs

--- classes/JavaScriptPluralsightSlide.class.php
+++ classes/JavaScriptPluralsightSlide.class.php
@@ -2,7 +2,7 @@
 class JavaScriptPluralsightSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='JavaScript' AND class='Pluralsight' ORDER BY rand();";
+        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='JavaScript' AND class='Pluralsight preview' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
         while($row = $stmt->fetch()) {
@@ -10,8 +10,8 @@ class JavaScriptPluralsightSlide extends Slider{
 			$alt = $row['alt'];
             $class = $row['class'];
             echo "<li class='item-a slide'>
-            <article>
-            <a href='./cert/BIG/$picture' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\"></a>
+            <article class='cert'>
+            <a href='./cert/BIG/$picture' data-href='./cert/SMALL/$picture' class='progressive replace' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\" loading=\"lazy\"></a>
             </article></li>";
             }
         }
