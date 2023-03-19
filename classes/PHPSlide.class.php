708f7ebd61db85a1bf5865eaf6be0120dd741461
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-20 07:12:39 +0900

    Added progressive images

--- classes/PHPSlide.class.php
+++ classes/PHPSlide.class.php
@@ -11,7 +11,7 @@ class PHPSlide extends Slider{
             $class = $row['class'];
             echo "<li class='item-a slide'>
             <article class='cert'>
-            <a href='./cert/BIG/$picture data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\" loading=\"lazy\"></a>
+            <a href='./cert/BIG/$picture' data-href='./cert/SMALL/$picture' class='progressive replace' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\" loading=\"lazy\"></a>
             </article></li>";
             }
         }
