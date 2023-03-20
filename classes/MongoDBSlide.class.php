bf40a6a088c4a1928358cfc72264dd7808834a03
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 02:16:16 +0900

    Added MongoDB slide class

--- classes/MongoDBSlide.class.php
+++ classes/MongoDBSlide.class.php
@@ -2,7 +2,7 @@
 class MongoDBSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='MongoDB' ORDER BY rand();";
+        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='MongoDB' OR skill='NoSQL' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
         while($row = $stmt->fetch()) {
@@ -10,8 +10,8 @@ class MongoDBSlide extends Slider{
 			$alt = $row['alt'];
             $class = $row['class'];
             echo "<li class='item-a slide'>
-            <article>
-            <a href='./cert/BIG/$picture' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\"></a>
+            <article class='cert'>
+            <a href='./cert/BIG/$picture' data-href='./cert/SMALL/$picture' class='progressive replace' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\" loading=\"lazy\"></a>
             </article></li>";
             }
         }
