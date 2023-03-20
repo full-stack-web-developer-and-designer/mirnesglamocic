1c21149ad42b5d18687d417c0e373496fb6e8001
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 03:47:27 +0900

    Added docker cert

--- docker.php
+++ docker.php
@@ -5,15 +5,15 @@ require_once 'core/init.php';
 <html lang="en">
 <head>
 <?php 
-	$title="Mirnes Glamočić | Database Certificates";
+	$title="Mirnes Glamočić | Docker Certificates";
     //Check length of title for SEO (50-70 characters) - 42
-    //echo "Length of title is " . strlen($title) . "characters long." . " ";
-    $og_title="Mirnes Glamočić | Database Certificates";
+    echo "Length of title is " . strlen($title) . "characters long." . " ";
+    $og_title="Mirnes Glamočić | Docker Certificates";
 	$website_url="https://mirnesglamocic.com";
-    $meta['description']="Mirnes Glamočić is an experienced Database Developer with several years of experience in the database development using relational databases and MongoDB.";
+    $meta['description']="Mirnes Glamočić is an experienced Full-stack Web Developer and Designer with several years of experience in the web development and design.";
     //Check length of description for SEO (140-180 characters) - 155
     echo "Length of description is " . strlen($meta['description']) . "characters long.";
-	$meta['keywords']="web development, database development, database developer, SQL Developer, SQL Development, MySQL Development, MySQL Developer, MongoDB Developer, MongoDB Development";
+	$meta['keywords']="web development, web design, ui/ux design, docker";
 	$og_image="./images/MirnesGlamocic.jpg";
 	$image_alt="Mirnes Glamočić";
 	include './inc/head.php';
@@ -26,18 +26,24 @@ include 'inc/header.php';
 	<div class="wrapper">
 	<h1>Docker Certificates</h1>
 	<section class="slider-container">
-        <div id="progress" style="text-align:center">
-	        In progress...
-        </div>
+	<div class="slideControls">
+            <a class="slidePrev arrow" href="#" aria-label="See the previous certificate">
+              <i class="fa fa-angle-left"></i>
+             </a>
+            <a class="slideNext arrow" href="#" aria-label="See the following certificate">
+              <i class="fa fa-angle-right"></i>
+            </a>
+      </div><!--end .slideControls -->
+		<ul class="content-slider">
+		<!-- slider box -->
+			<?php
+				$docker = new DockerSlide();
+				$docker->renderSlider();
+			?>
+		</ul>
 	</section><!--end .slider-container-->
-
-	</div><!--end .wrapper-->
-
-<script src="lightbox2/src/js/lightbox.js"></script>
-<script src='./JS/slider.js'></script>
+</div><!--end .wrapper-->
 <?php
 	include_once './inc/footer.php';
 	//end #footer
-?>  
-</body>
-</html>
+?>
