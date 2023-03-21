a51b79ba06dd0ad886b569887bd7f9b49b6ab19c
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 21:51:18 +0900

    Changed DB name

--- classes/Slider.class.php
+++ classes/Slider.class.php
@@ -2,7 +2,7 @@
 class Slider extends ConnectSlider {
 	// Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-	$sql = "SELECT * FROM mirnesglamocic_cert.certifications ORDER BY rand();";
+	$sql = "SELECT * FROM mirnesgl_cert.certifications ORDER BY rand();";
 	$stmt = $this->__connect()->query($sql);
 	
 	while($row = $stmt->fetch()) {
