8a1891d793e158c16b9c9cf78e949e098f954d0e
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 21:35:26 +0900

    Removed unused code

--- classes/Page.class.php
+++ classes/Page.class.php
@@ -14,11 +14,5 @@ class Page{
 	public static function init(){
 		self::$db = Connect::getInstance();
 	}
-/*public function render(){
-		$render = "<div><h1>" . $this->title . "</h1>";
-		$render .= "<p><strong>" . $this->strong . "</strong>" . $this->first_part . "<br>" . $this->second_part . "</p>";
-		$render .= "<p>". $this->third_part . "<span class='none'>" . $this->span_none . "</p></div>";
-		return $render;
-	}*/
-	}
+}
 	
