d777cc701cf6dad146e06205030135dbdc214d9f
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 20:39:15 +0900

    Changed db name

--- classes/PythonSlide.class.php
+++ classes/PythonSlide.class.php
@@ -2,7 +2,7 @@
 class PythonSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='Python' ORDER BY rand();";
+        $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='Python' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
         while($row = $stmt->fetch()) {
