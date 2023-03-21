fbe07eea7153c72ea17299d600be6b8a65356f05
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 22:25:26 +0900

    Rwmoved double loop of imgs

--- JS/slider.js
+++ JS/slider.js
@@ -1,5 +1,5 @@
 'use strict';
-jQuery(document).ready(function () {
+jQuery(document).ready(function() {
   jQuery('.content-slider').each(function(i, e){
     var id = 'adaptive';
     jQuery(e).attr('id', id+i);
@@ -8,10 +8,45 @@ jQuery(document).ready(function () {
       item:4,
       slideMove: 1,
       slideMargin: 15,
-      loop: true,
       keyPress: true,
-      pager:false,
-      gallery: false,
+      pager: false,
+      gallery: true,
+      responsive: [          {
+        breakpoint:1260,
+        settings: {
+            item:4,
+            slideMove:1,
+            slideMargin:12,
+          }
+    },			        {
+        breakpoint:1118,
+        settings: {
+            item:3,
+            slideMove:1,
+            slideMargin:10,
+          }
+    },	{
+        breakpoint:1006,
+        settings: {
+            item:2,
+            slideMove:1,
+            slideMargin:15,
+          }
+    },        {
+        breakpoint:700,
+        settings: {
+            item:2,
+            slideMove:1,
+            slideMargin:10,
+          }
+    },			  {
+        breakpoint:520,
+        settings: {
+            item:1,
+            slideMove:1,
+            slideMargin:0,
+          }
+    }],
      });
 });
 });
