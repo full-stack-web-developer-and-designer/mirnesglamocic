ad6f1ba53488a2f5b69a9f299f527473b565b9e8
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 21:54:22 +0900

    Changed DB name

--- classes/TypeScriptSlide.class.php
+++ classes/TypeScriptSlide.class.php
@@ -2,7 +2,7 @@
 class TypeScriptSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='TypeScript' ORDER BY rand();";
+        $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='TypeScript' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
         while($row = $stmt->fetch()) {
