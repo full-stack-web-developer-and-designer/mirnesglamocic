459d4c44e99d5b51ceef86eb3d42835e328e737e
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 21:50:15 +0900

    Changed DB name

--- classes/Skills.class.php
+++ classes/Skills.class.php
@@ -2,7 +2,7 @@
 class Skills extends ConnectSlider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesglamocic_cert.skills;";
+        $sql = "SELECT * FROM mirnesgl_cert.skills;";
         $stmt = $this->__connect()->query($sql);
        
         while($row = $stmt->fetch()) {
