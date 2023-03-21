b702152bd507e1f6fc33c52e65db454241bb378d
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 21:56:17 +0900

    Changed DB name

--- classes/UXFoundationSlide.class.php
+++ classes/UXFoundationSlide.class.php
@@ -2,7 +2,7 @@
 class UXFoundationSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='UX Foundations' ORDER BY rand();";
+        $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='UX Foundations' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
         while($row = $stmt->fetch()) {
