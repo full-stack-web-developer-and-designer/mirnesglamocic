4ddb51a2f3a8706d6c6f9510d2ccb77afb7e7777
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 22:18:02 +0900

    Changed DB name

--- classes/WordPressPluginSlide.class.php
+++ classes/WordPressPluginSlide.class.php
@@ -2,7 +2,7 @@
 class WordPressPluginSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='WordPressPlugin' ORDER BY rand();";
+        $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='WordPressPlugin' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
         while($row = $stmt->fetch()) {
@@ -11,7 +11,7 @@ class WordPressPluginSlide extends Slider{
             $class = $row['class'];
             echo "<li class='item-a slide'>
             <article class='cert'>
-            <a href='./cert/BIG/$picture' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\" loading=\"lazy\"></a>
+            <a href='./cert/BIG/$picture' data-href='./cert/SMALL/$picture' class='progressive replace' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\" loading=\"lazy\"></a>
             </article></li>";
             }
         }
