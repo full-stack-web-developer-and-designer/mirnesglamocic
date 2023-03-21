d5fd9517fcd47bb5265d76399dd402556e6ceb45
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 20:48:07 +0900

    Changed DB name

--- classes/CSSSlide.class.php
+++ classes/CSSSlide.class.php
@@ -2,7 +2,7 @@
 class CSSSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='CSS' OR skill='CSS WebDesign' ORDER BY rand();";
+        $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='CSS' OR skill='CSS WebDesign' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
         while($row = $stmt->fetch()) {
