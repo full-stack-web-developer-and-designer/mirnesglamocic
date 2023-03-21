41b8a7aed3f49206bac797afb77beb739b5ee4cd
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 22:09:13 +0900

    Changed DB name

--- classes/WebDesignSlide.class.php
+++ classes/WebDesignSlide.class.php
@@ -2,7 +2,7 @@
 class WebDesignSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='WebDesign' OR skill='UX Web Design' OR skill='JavaScript WebDesign' OR skill='HTML WebDesign' OR skill='HTMLandCSS WebDesign' OR skill='CSS WebDesign' OR skill='WebServices WebDesign' OR skill='Bootstrap WebDesign' OR skill='SASS WebDesign' OR skill='WebDevelopment WebDesign' ORDER BY rand();";
+        $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='WebDesign' OR skill='UX Web Design' OR skill='JavaScript WebDesign' OR skill='HTML WebDesign' OR skill='HTMLandCSS WebDesign' OR skill='CSS WebDesign' OR skill='WebServices WebDesign' OR skill='Bootstrap WebDesign' OR skill='SASS WebDesign' OR skill='WebDevelopment WebDesign' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
         while($row = $stmt->fetch()) {
