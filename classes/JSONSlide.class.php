c3243b6d4a7c6791e48e878ba3dccaebf3419df6
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 21:23:57 +0900

    Changed DB name

--- classes/JSONSlide.class.php
+++ classes/JSONSlide.class.php
@@ -2,7 +2,7 @@
 class JSONSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='JSON XML' OR skill='JavaScript JSON' AND class='LinkedIn preview' ORDER BY rand();";
+        $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='JSON XML' OR skill='JavaScript JSON' AND class='LinkedIn preview' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
         while($row = $stmt->fetch()) {
