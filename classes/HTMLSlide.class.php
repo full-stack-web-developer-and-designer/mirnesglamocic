6ec31aa9dbc5020aadc79b0dfaf11c952a23b87f
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-03-21 21:07:07 +0900

    Changed DB name

--- classes/HTMLSlide.class.php
+++ classes/HTMLSlide.class.php
@@ -2,7 +2,7 @@
 class HTMLSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesglamocic_cert.certifications WHERE skill='HTML' OR skill='HTML WebDesign' ORDER BY rand();";
+        $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='HTML' OR skill='HTML WebDesign' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
         while($row = $stmt->fetch()) {
