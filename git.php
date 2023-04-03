d1eb4249cc465d9feedc9847e7b039581e7f7921
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-04-03 05:38:33 +0200

    Added og image and removed custom arrows

--- git.php
+++ git.php
@@ -4,18 +4,17 @@ require_once 'core/init.php';
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php 
-	$title="Mirnes Glamočić | Version Control Certificates";
-    //Check length of title for SEO (50-70 characters) - 48
-    //echo "Length of title is " . strlen($title) . "characters long." . " ";
-    $og_title="Mirnes Glamočić | Git Certificates";
-	$website_url="https://mirnesglamocic.com";
-    $meta['description']="Mirnes Glamočić is an experienced Full-stack Web Developer with several years of experience in the complete process of web development and design.";
-    //Check length of description for SEO (140-180 characters) - 148
-    //echo "Length of description is " . strlen($meta['description']) . "characters long.";
-	$meta['keywords']="web development, web design, version control, git, github";
-	$og_image="./images/MirnesGlamocic.jpg";
-	$image_alt="Mirnes Glamočić";
+<?php
+	$fb_title="Version Control Certificates by Mirnes Glamočić";
+	$meta['og_description']="Mirnes Glamočić is an experienced Full-stack Web Developer with with extensive experience using GIT.";
+	$og_url="https://mirnesglamocic.com/gitcertificates.html";
+	$og_image="https://mirnesglamocic.com/images/git_og.jpg";
+	$og_image_alt=$fb_title;
+	$linkedin_title=$fb_title;
+	$title=$fb_title;
+	$meta['description']="Mirnes Glamočić is an experienced Full-stack Web Developer with with extensive experience using GIT. Also, he is experienced in web design and various operating systems.";
+	$twitter_image="https://mirnesglamocic.com/images/git_og.webp";
+	$meta['keywords']="web development, web developer, version control, version control system, git, github, bitbucket";
 	include './inc/head.php';
 ?>
 </head>
@@ -27,14 +26,6 @@ include 'inc/header.php';
 	<h1>Version Control Certificates</h1>
 	
 	<section class="slider-container">
-	<div class="slideControls">
-            <a class="slidePrev arrow" href="#" aria-label="See the previous certificate">
-              <i class="fa fa-angle-left"></i>
-             </a>
-            <a class="slideNext arrow" href="#" aria-label="See the following certificate">
-              <i class="fa fa-angle-right"></i>
-            </a>
-      </div><!--end .slideControls -->
 		<ul class="content-slider">
 		<!-- slider box -->
 			<?php
@@ -47,14 +38,6 @@ include 'inc/header.php';
 	</section><!--end .slider-container-->
 
 	<section class="slider-container">
-	<div class="slideControls">
-            <a class="slidePrev arrow" href="#" aria-label="See the previous certificate">
-              <i class="fa fa-angle-left"></i>
-             </a>
-            <a class="slideNext arrow" href="#" aria-label="See the following certificate">
-              <i class="fa fa-angle-right"></i>
-            </a>
-      </div><!--end .slideControls -->
 		<ul class="content-slider">
 		<!-- slider box -->
 			<?php
