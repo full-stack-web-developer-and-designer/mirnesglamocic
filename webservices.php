b71644dcd87f1fc8e5f7c042b188257d2da8a147
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-04-03 04:34:03 +0200

    Added og image and removed custom arrows

--- webservices.php
+++ webservices.php
@@ -4,18 +4,17 @@ require_once 'core/init.php';
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php 
-	$title="Mirnes Glamočić | Certificates about Web Services";
-    //Check length of title for SEO (50-70 characters) - 42
-    //echo "Length of title is " . strlen($title) . "characters long." . " ";
-    $og_title="Mirnes Glamočić | Certificates about Web Services";
-	$website_url="https://mirnesglamocic.com";
-    $meta['description']="Mirnes Glamočić is an experienced Web Developer and Designer with several years of experience in the web development and design industry.";
-    //Check length of description for SEO (140-180 characters) - 139
-    //echo "Length of description is " . strlen($meta['description']) . "characters long.";
-	$meta['keywords']="web development, web developer, web services, web design, web designer";
-	$og_image="./images/MirnesGlamocic.jpg";
-	$image_alt="Mirnes Glamočić";
+<?php
+	$fb_title="Web Services Certificates by Mirnes Glamočić";
+	$meta['og_description']="Mirnes Glamočić has experience in various web services for Web and Rich Internet Applications.";
+	$og_url="https://mirnesglamocic.com/webservicescertificates.html";
+	$og_image="https://mirnesglamocic.com/images/webservices_og.jpg";
+	$twitter_image="https://mirnesglamocic.com/images/webservices_og.webp";
+	$og_image_alt=$fb_title;
+	$linkedin_title=$fb_title;
+	$title=$fb_title;
+	$meta['description']="Mirnes Glamočić has experience in various web services for Web and Rich Internet Applications. He also has experience in full-stack web development and design.";
+	$meta['keywords']="linux, ubuntu, debian, centos, fedora, windows, web development, software development, operating systems";
 	include './inc/head.php';
 ?>
 </head>
@@ -27,14 +26,6 @@ include 'inc/header.php';
 	<h1>Web Services Certificates</h1>
 	
 	<section class="slider-container">
-	<div class="slideControls">
-	<a class="slidePrev arrow" href="#" aria-label="See the previous certificate">
-              <i class="fa fa-angle-left"></i>
-             </a>
-            <a class="slideNext arrow" href="#" aria-label="See the following certificate">
-              <i class="fa fa-angle-right"></i>
-            </a>
-      </div><!--end .slideControls -->
 		<ul class="content-slider">
 		<!-- slider box -->
 			<?php
@@ -45,14 +36,6 @@ include 'inc/header.php';
 	</section><!--end .slider-container-->
 
 	<section class="slider-container">
-	<div class="slideControls">
-		<a class="slidePrev arrow" href="#" aria-label="See the previous certificate">
-            <i class="fa fa-angle-left"></i>
-        </a>
-        <a class="slideNext arrow" href="#" aria-label="See the following certificate">
-            <i class="fa fa-angle-right"></i>
-        </a>
-      </div><!--end .slideControls -->
 		<ul class="content-slider">
 		<!-- slider box -->
 			<?php
