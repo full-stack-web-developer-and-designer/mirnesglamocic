d1a3fc2649421422d89228e81e2460f6c251d498
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2023-04-06 04:43:11 +0200

    Moved script tags at the bottom

--- inc/footer.php
+++ inc/footer.php
@@ -15,10 +15,25 @@ echo copyright(2023);
 ?>
 </footer>
 <?php echo "<p class=\"barcode\">This page was printed from: ".$url;?></p>
-
-<script src="https://mirnesglamocic.com/lightbox2/src/js/lightbox.min.js"></script>
-<script src='https://mirnesglamocic.com/JS/slider.min.js'></script>
+<!-- responsive navigation -->
+<script>
+var navigation=$.noConflict();navigation(document).ready(function(n){var e=n("#respmenu"),i=n("#nav>ul");n(e).on("click",function(n){n.preventDefault(),i.toggleClass("open")}),n(window).resize(function(){n(window).width()>768&&i.is(":hidden")&&i.removeAttr("style")})});
+</script>
+<script defer>
+window.onorientationchange=function(){var a=window.orientation;switch(a){case 0:case 90:case -90:window.location.reload()}};
+</script>
+<!-- include fontawesome if cdn fails -->
+<script>
+window.onload=function(){if(!window.FontAwesome){var e=document.createElement("script");e.type="text/javascript",e.src="/JS/fontawesome.min.js",document.body.appendChild(e)}};
+</script>
+<!--jQuery validate plugin -->
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
+<script>
+	window.jQuery || document.write("<script src='./JS/jquery.validate.min.js'><\/script>");
+</script>
+<script src="./lightbox2/src/js/lightbox.min.js"></script>
+<script src='./JS/slider.min.js'></script>
 <script src="https://cdn.jsdelivr.net/npm/progressive-image.js/dist/progressive-image.js" async></script>
-<script src="https://mirnesglamocic.com/JS/lazyloading.min.js"></script>
+<script src="./JS/lazyloading.min.js"></script>
 </body>
 </html>
--- inc/head.php
+++ inc/head.php
@@ -3,7 +3,7 @@ Wow! Welcome to my code. If you need a professional website don't hesitate to co
 -->
 <head id="head">
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php echo $title; ?></title>
 <!-- meta tags -->
 <meta name="robots" content="index,follow">
@@ -39,53 +39,43 @@ Wow! Welcome to my code. If you need a professional website don't hesitate to co
 <meta itemprop="description" content="<?php echo $meta['description'];?>">
 <meta itemprop="image" content="Mirnes Glamočić">
 <!-- include google fonts -->
-<link rel="dns-prefetch" href="//fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Roboto:400,400i,700,700i|Dancing+Script&display=swap&amp;subset=latin-ext" rel="stylesheet">
+<link rel="dns-prefetch" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Roboto:400,400i,700,700i&display=swap&amp;subset=latin-ext" rel="stylesheet">
 <!-- favicon -->
+<link rel="android-chrome-icon" sizes="192x192" href="/favicon/android-chrome-192x192.png">
 <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
 <link rel="manifest" href="/favicon/site.webmanifest">
 <link rel="canonical" href="https://mirnesglamocic.com/" />
-<!-- include fontawesome --> 
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-<script src="https://kit.fontawesome.com/c3fb2ab6eb.js" crossorigin="anonymous"></script>
 <!-- Custom CSS -->
 <link rel="stylesheet" href="/CSS/style.css" media="all"/>
+<!-- lightslider CSS-->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/css/lightslider.min.css" rel="stylesheet"/>
+<!-- lightbox css -->
+<link rel="stylesheet" type="text/css" href="./lightbox2/src/css/lightbox.min.css">
+<!-- include fontawesome  -->
+<link rel="preload" as="style" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" onload="this.rel='stylesheet'" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/progressive-image.js/dist/progressive-image.css">
+<script src="https://kit.fontawesome.com/c3fb2ab6eb.js" crossorigin="anonymous"></script>
 <!-- jQuery -->
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script>
 	window.jQuery || document.write("<script src='./JS/jQuery-1.7.2.min.js'><\/script>");
 </script>
-<!-- lightslider CSS-->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/css/lightslider.min.css" rel="stylesheet"/>
-<!-- lightslider js-->
-<script defer src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script defer src="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/js/lightslider.min.js"></script>
-<!-- lightbox css -->
-<link rel="stylesheet" type="text/css" href="./lightbox2/src/css/lightbox.min.css">
-<!--jQuery validate plugin -->
-<script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
 <noscript><p class="errorJS">To display the page correctly, please enable JavaScript in your browser!</p></noscript>
-<!-- responsive navigation -->
-<script>
-var navigation=$.noConflict();navigation(document).ready(function(n){var e=n("#respmenu"),i=n("#nav>ul");n(e).on("click",function(n){n.preventDefault(),i.toggleClass("open")}),n(window).resize(function(){n(window).width()>768&&i.is(":hidden")&&i.removeAttr("style")})});
-</script>
-<script defer>
-window.onorientationchange=function(){var a=window.orientation;switch(a){case 0:case 90:case -90:window.location.reload()}};
-</script>
 <!-- picturefill -->
 <script>
     // Picture element HTML5 shiv
     document.createElement( "picture" );
-  </script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/picturefill/3.0.2/picturefill.min.js" integrity="sha512-Nh/FthCqOFq56kGp1DsNb6GKK29iIv2ZJc7Fylln1WRrWpeE5LjSBWeAp61dkBASDxZT12aL9mZyIrhsAqgRpw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script>
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/picturefill/3.0.2/picturefill.min.js" integrity="sha512-Nh/FthCqOFq56kGp1DsNb6GKK29iIv2ZJc7Fylln1WRrWpeE5LjSBWeAp61dkBASDxZT12aL9mZyIrhsAqgRpw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
 	window.jQuery || document.write("<script src='./JS/picturefill.min.js'><\/script>");
-  </script>
+</script>
 <!-- lazy loading images -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/progressive-image.js/dist/progressive-image.css">
 <script src="https://unpkg.com/webp-hero@0.0.0-dev.21/dist-cjs/polyfills.js"></script>
 <script src="https://unpkg.com/webp-hero@0.0.0-dev.21/dist-cjs/webp-hero.bundle.js"></script>
 <script>
