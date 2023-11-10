4847c1b2251d7d6aceca8742dbb194db4b510ae1
Mirnes Glamočić <contact@mirnesglamocic.com>
2023-11-10 01:46:58 +0100

    Added aria label for navigation

--- classes/Navigations.class.php
+++ classes/Navigations.class.php
@@ -4,12 +4,12 @@ class Navigations extends Entity{
 	public static $keyColumn = 'id';
 	
 	public function render(){
-		$render = "<li><a href=\"/#aboutme\">$this->about</a><li> ";
-        $render .= "<li><a href=\"/#certificates\">$this->certificates</a></li>";
-        $render .= "<li><a href=\"/#skills\">$this->skills</a></li>";
-        $render .= "<li><a href=\"/#projects\">$this->projects</a></li>";
-        $render .= "<li><a href=\"/#services\">$this->services</a></li>";
-        $render .= "<li><a href=\"/#contact\">$this->contact</a></li>";
+		$render = "<li><a href=\"/#aboutme\" aria-label=\"about me\">$this->about</a><li> ";
+        $render .= "<li><a href=\"/#certificates\" aria-label=\"my certificates\">$this->certificates</a></li>";
+        $render .= "<li><a href=\"/#skills\" aria-label=\"my skills\">$this->skills</a></li>";
+        $render .= "<li><a href=\"/#projects\" aria-label=\"my projects\">$this->projects</a></li>";
+        $render .= "<li><a href=\"/#services\" aria-label=\"my services\">$this->services</a></li>";
+        $render .= "<li><a href=\"/#contact\" aria-label=\"contact me\">$this->contact</a></li>";
 		return $render;
 	}
 }
