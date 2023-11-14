3a81d9181240246f1989bb697dde117253011dfc
Mirnes Glamočić <contact@mirnesglamocic.com>
2023-11-14 02:30:00 +0100

    Improved SQL query for select only new cert by Linkedin

--- classes/CSSnewSlide.class.php
+++ classes/CSSnewSlide.class.php
@@ -2,7 +2,7 @@
 class CSSnewSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='CSS' AND class='LinkedInNew' OR skill='CSS WebDesign' ORDER BY rand();";
+        $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='CSS' AND class='LinkedInNew' OR skill='CSS WebDesign' AND class='LinkedInNew' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
         while($row = $stmt->fetch()) {
