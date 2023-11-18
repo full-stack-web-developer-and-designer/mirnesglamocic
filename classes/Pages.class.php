466972931a5a2236429172421f959ae55b83bfe2
Mirnes Glamočić <contact@mirnesglamocic.com>
2023-11-18 04:08:47 +0100

    Removed unnecessary class white for h2 tag

--- classes/Pages.class.php
+++ classes/Pages.class.php
@@ -10,7 +10,7 @@ class Pages extends Entity{
         <div class='wrapper'><img src='images/$this->my_image' alt='$this->alt' class='imgLeft'><h2>" . $this->title . "</h2><p>" . $this->pages_description . "</p></div></article>";
 		$render .= "<article id='about' class='white'><div class='wrapper'>
             <div id='quotes'>
-                <h2 class='white'>$this->title_quotes</h2><p>$this->quote_1<em>$this->Picasso</em></p><br class='none'><p>$this->quote_2<em>$this->Gandhi</em></p><br class='none'><p>$this->quote_3<em>$this->Confucius</em></div></div></article>";
+                <h2>$this->title_quotes</h2><p>$this->quote_1<em>$this->Picasso</em></p><br class='none'><p>$this->quote_2<em>$this->Gandhi</em></p><br class='none'><p>$this->quote_3<em>$this->Confucius</em></div></div></article>";
 		return $render;
 	}
 	}
