2cb65bd98f223d1a45ac74ca5f03b2931d919a4b
Mirnes Glamočić <contact@mirnesglamocic.com>
2023-12-30 23:57:49 +0100

    Removed class footer_comma2 and footer_none2

--- inc/footer.php
+++ inc/footer.php
@@ -7,9 +7,9 @@ function copyright($startYear) {
 	if ($startYear < $currentYear) {
 		$currentYear = date('y');
 		return "<p class='no_print'><small><span class='copyear'>&copy; Copyright $startYear &ndash; $currentYear" . "</span><span class='footer_comma'>,</span><span class='footer_none'> | 
-		</span><a id='footerlink' href='https://mirnesglamocic.com'>Mirnes Glamočić</a><span class='footer_comma2'>,</span><span class='footer_none2'> | </span><span id='copyrighter'>Web Developer, UI/UX and Web Designer</span></small></p>";
+		</span><br id='none'><a id='footerlink' href='https://mirnesglamocic.com'>Mirnes Glamočić</a><span class='footer_comma'>,</span><span class='footer_none'> | </span><span id='copyrighter'>Web Developer, UI/UX and Web Designer</span></small></p>";
 	} else {
-		return "<p class='no_print'><small><span class='copyear'>&copy; Copyright $startYear " . " </span><span class='footer_comma'>,</span><span class='footer_none'> | </span><a id='footerlink' href='https://mirnesglamocic.com'>Mirnes Glamočić</a><span class='footer_comma2'>,</span><span class='footer_none2'> | </span><span id='copyrighter'>Web Developer, UI/UX and Web Designer</span></small></p>";
+		return "<p class='no_print'><small><span class='copyear'>&copy; Copyright $startYear" . "</span><span class='footer_comma'>,</span><span class='footer_none'> | </span><br id='none'><a id='footerlink' href='https://mirnesglamocic.com'>Mirnes Glamočić</a><span class='footer_comma'>,</span><span class='footer_none'> | </span><span id='copyrighter'>Web Developer, UI/UX and Web Designer</span></small></p>";
 	}
 }
 echo copyright(2023);
