c8a90818e38d7b62bf59f3a939c4c194c28ab3d4
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-01-10 19:45:16 +0100

    Created PHP class for SVG certificates

---
+++ classes/SvgSlide.class.php
@@ -0,0 +1,18 @@
+<?php
+class SvgSlide extends Slider{
+    // Fetch data from MySQL using PDO - PHP Data Object	
+	public function renderSlider(){
+        $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='SVG' OR skill='SVG Illustrator' ORDER BY rand();";
+        $stmt = $this->__connect()->query($sql);
+
+        while($row = $stmt->fetch()) {
+        	$picture = $row['picture'];
+			$alt = $row['alt'];
+            $class = $row['class'];
+            echo "<li class='item-a slide'>
+            <article class='cert'>
+            <a href='./cert/BIG/$picture' data-href='./cert/SMALL/$picture' class='progressive replace' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\" loading=\"lazy\"></a>
+            </article></li>";
+            }
+        }
+    }
