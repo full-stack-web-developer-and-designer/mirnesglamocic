d1fb38f7bd1b5a22018d3867fab06c811906df22
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-01-10 20:10:21 +0100

    Moved favicons to root directory

--- inc/head.php
+++ inc/head.php
@@ -43,10 +43,10 @@ Wow! Welcome to my code. If you need a professional website don't hesitate to co
 <link rel="dns-prefetch" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Roboto:400,400i,700,700i&display=swap&amp;subset=latin-ext" rel="stylesheet">
 <!-- favicon -->
-<link rel="android-chrome-icon" sizes="192x192" href="/favicon/android-chrome-192x192.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+<link rel="android-chrome-icon" sizes="192x192" href="./android-chrome-192x192.png">
+<link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
 <link rel="manifest" href="/favicon/site.webmanifest">
 <link rel="canonical" href="https://mirnesglamocic.com/" />
 <!-- Custom CSS -->
