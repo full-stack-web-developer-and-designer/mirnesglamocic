2ab3ddadc78f4efc3e3190e8389b4f3a33c2c324
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-01-15 16:45:13 +0100

    Made shorter title for mobiles and added samsung logo to projects

--- index.php
+++ index.php
@@ -14,45 +14,27 @@
 
 <?php
 
-    $fb_title="Website Development, UI/UX and Web Design services by certified IT expert";
-
-    $meta['og_description']="Professional website development, UI/UX and web design services by certified IT expert  from Bosnia and Herzegovina.";
-    //Check length of description for SEO (max 120 characters)-116
-    //echo "Length of og description is " . strlen($meta['og_description']) . "characters long.";
+    $fb_title="Website Development and Web Design services by certified IT expert";
+    $meta['og_description']="Professional website development, UI/UX and web design services by certified IT expert from Bosnia and Herzegovina.";
     $og_url="https://mirnesglamocic.com";
-
     $og_image="https://mirnesglamocic.com/images/Mirnes_og.jpg";
-
-    $og_image_alt="Full-Stack Web Developer and Desiger Mirnes Glamočić";
-
+    $og_image_alt="Mirnes Glamočić | Full-Stack Web Developer and Designer, Bosnia & Herzegovina";
 	$linkedin_title="Website Development, UI/UX and Web Design services by certified IT expert";
-
-    $title="Website Development, UI/UX and Web Design services by certified IT expert | Mirnes Glamočić";
-
-    $meta['description']="Professional website development, UI/UX and web design services by Mirnes Glamočić from Bosnia and Herzegovina, certified by several IT organizations.";
-    //Check length of description for SEO (120-158 characters)-152
-    //echo "Length of description is " . strlen($meta['description']) . "characters long.";
+    $title="Mirnes Glamočić | Expert Web Developer and Designer";
+    $meta['description']=$meta['og_description'];
     $twitter_image="https://mirnesglamocic.com/images/MirnesGlamocic.webp";
-
-	$meta['keywords']="portfolio, web development, web developer, front-end development, frontend development, front-end developer, frontend developer, backend-development, backend development, back-end developer, backend developer, database development database developer, web design, web designer, the best web developer, the best web designer";
 
 	include './inc/head.php';
 
 ?>
-
-    <?php
-
-        $rnd = rand(1,2);
-
-        $pos = ($rnd == 1) ? "left;" : ($rnd == 2) ? "right;" : "top";
-
-    ?>
-
+<?php
+    $rnd = rand(1,2);
+?>
 <style type="text/css">
 
     /* Hero img */
 
-    #hero.lazy {
+    .hero.lazy {
         background-image: none;
         background-color: #F1F1FA;
     }
@@ -66,9 +48,11 @@
         -moz-background-size: cover;
         -o-background-size: cover;
         background-size: cover;
-        min-height: 100%;
-        padding: 180px 0;
+        padding: 185px 0;
         text-align: center;
+    }
+    h1.white{
+        margin:0 auto;
     }
 
 @media screen and (min-width: 1024px) and (max-width: 1260px) {
@@ -85,18 +69,15 @@
 
     #hero {
         background-image: url("/images/bg<?php echo $rnd ?>_1023.webp");
+        background-attachment:inherit;
     }
 
 }
 
     @media screen and (max-width: 767px) {
-
         #hero {
-
-            background-attachment: scroll;
-
-            background-position: top <?php echo $pos ?>;
-
+        background-position: top right;
+        background-attachment:inherit;
         }
 
         /* section project for mockup sites */
@@ -122,11 +103,12 @@
     @media screen and (min-width: 409px) and (max-width: 767px) {
 
         #hero {
-
-            background-image: url("/images/bg767_<?php echo $rnd ?>.webp");
-
+            background-image: url("/images/bg<?php echo $rnd ?>_767.webp");
+            background-size:767px 900;
             padding: 35px;
-
+        }
+        h1.white{
+            margin: 150px auto;
         }
 
     }
@@ -134,23 +116,13 @@
     @media screen and (max-width: 408px) {
 
         #hero {
-
-            background-image: url("/images/bg408_<?php echo $rnd ?>.webp");
-
+            background-image: url("/images/bg<?php echo $rnd ?>_408.webp");
             padding: 20px;
-
         }
 
-        h1 { padding: 25px;}
-
-    }
-
-    @supports (-webkit-overflow-scrolling: touch) {
-
-        #hero {
-
-            background-attachment: scroll;
-
+        h1.white{
+            margin: 140px auto;
+            padding: 25px;
         }
 
     }
@@ -177,7 +149,10 @@
 
     }
 
-    @media screen and (max-width: 767px) and (min-device-pixel-ratio: 2), screen and (max-width: 767px) and (-webkit-min-device-pixel-ratio: 2){
+    @media screen and (max-width: 767px) and (min--moz-device-pixel-ratio: 2),
+    screen and (max-width: 767px) and (o-min-device-pixel-ratio: 2),
+    screen and (max-width: 767px) and (-webkit-min-device-pixel-ratio: 2),
+    screen and (max-width: 767px) and (min-device-pixel-ratio: 2){
 
         #hero {
 
@@ -186,10 +161,10 @@
         }
 
     }
-
-    @media screen and (max-width: 408px) and (min-device-pixel-ratio: 2),
-
-    screen and (max-width: 408px) and (-webkit-min-device-pixel-ratio: 2) {
+    @media screen and (max-width: 408px) and (min--moz-device-pixel-ratio: 2),
+    screen and (max-width: 408px) and (o-min-device-pixel-ratio: 2),
+    screen and (max-width: 408px) and (-webkit-min-device-pixel-ratio: 2),
+    screen and (max-width: 408px) and (min-device-pixel-ratio: 2){
 
         #hero {
 
@@ -213,22 +188,10 @@
 
     }
 
-    @media screen and (max-width: 1023px) and (min-device-pixel-ratio: 3),
-
-    screen and (max-width: 1023px) and (-webkit-min-device-pixel-ratio: 3) {
-
-        #hero {
-
-            background-image: url("/images/bg<?php echo $rnd ?>_1023@3x.webp");
-
-        }
-
-    }
-
-    @media screen and (max-width: 767px) and (min-device-pixel-ratio: 3),
-
-    screen and (max-width: 767px) and (-webkit-min-device-pixel-ratio: 3) {
-
+    @media screen and (max-width: 767px) and (min--moz-device-pixel-ratio: 3),
+    screen and (max-width: 767px) and (o-min-device-pixel-ratio: 3),
+    screen and (max-width: 767px) and (-webkit-min-device-pixel-ratio: 3),
+    screen and (max-width: 767px) and (min-device-pixel-ratio: 3){
         #hero {
 
             background-image: url("/images/bg<?php echo $rnd ?>_767@3x.webp");
@@ -237,9 +200,10 @@
 
     }
 
-    @media screen and (max-width: 408px) and (min-device-pixel-ratio: 3),
-
-    screen and (max-width: 408px) and (-webkit-min-device-pixel-ratio: 3) {
+    @media screen and (max-width: 408px) and (min--moz-device-pixel-ratio: 3),
+    screen and (max-width: 408px) and (o-min-device-pixel-ratio: 3),
+    screen and (max-width: 408px) and (-webkit-min-device-pixel-ratio: 3),
+    screen and (max-width: 408px) and (min-device-pixel-ratio: 3){
 
         #hero {
 
@@ -250,30 +214,6 @@
     }
 
     /* CSS query device pixel ratio: 4 */
-
-    @media screen and (max-width: 1260px) and (min-device-pixel-ratio: 4),
-
-    screen and (max-width: 1260px) and (-webkit-min-device-pixel-ratio: 4) {
-
-        #hero {
-
-            background-image: url("/images/bg<?php echo $rnd ?>_1260@4x.webp");
-
-        }
-
-    }
-
-    @media screen and (max-width: 1023px) and (min-device-pixel-ratio: 4),
-
-    screen and (max-width: 1023px) and (-webkit-min-device-pixel-ratio: 4) {
-
-        #hero {
-
-            background-image: url("/images/bg<?php echo $rnd ?>_1023@4x.webp");
-
-        }
-
-    }
 
     @media screen and (max-width: 767px) and (min-device-pixel-ratio: 4),
 
@@ -286,11 +226,10 @@
         }
 
     }
-
-    @media screen and (max-width: 408px) and (min-device-pixel-ratio: 4),
-
-    screen and (max-width: 408px) and (-webkit-min-device-pixel-ratio: 4) {
-
+    @media screen and (max-width: 408px) and (min--moz-device-pixel-ratio: 4),
+    screen and (max-width: 408px) and (o-min-device-pixel-ratio: 4),
+    screen and (max-width: 408px) and (-webkit-min-device-pixel-ratio: 4),
+    screen and (max-width: 408px) and (min-device-pixel-ratio: 4){
         #hero {
 
             background-image: url("/images/bg<?php echo $rnd ?>_408@4x.webp");
@@ -312,28 +251,36 @@
         include 'inc/header.php';
 
     ?>
+    <article id='hero'>
+        <div class='wrapper'><h1 class='white'>Web Development, UI/UX and Web Design</h1></div>
+    </article>
+<article id='aboutme'>
+<div class='wrapper'>
+    <picture class="imgLeft">
+     <!--[if IE 9]><video style='display: none;'><![endif]-->
+        <source srcset="./images/webdev.webp, ./images/webdev@2x.webp 2x, ./images/webdev@3x.webp 3x, ./images/webdev@4x.webp 4x" type="image/webp" width="310" height="310" loading="lazy" media="(min-width: 1261px)">
+        <source srcset="./images/webdev_1023.webp, ./images/webdev_1023@2x.webp 2x, ./images/webdev_1023@3x.webp 3x, ./images/webdev_1023@4x.webp 4x" type="image/webp" width="250" height="250" loading="lazy" media="(min-width: 1023px)">
+        <source srcset="./images/webdev_861.webp, ./images/webdev_861@2x.webp 2x, ./images/webdev_861@3x.webp 3x, ./images/webdev_861@4x.webp 4x" type="image/webp" width="200" height="200" loading="lazy" media="(min-width: 861px)">
+        <source srcset="./images/webdev_768.webp, ./images/webdev_768@2x.webp 2x, ./images/webdev_768@3x.webp 3x, ./images/webdev_768@4x.webp 4x" type="image/webp" width="150" height="150" loading="lazy" media="(min-width: 768px)">
+    <!--[if IE 9]></video><![endif]-->
+    <img decoding="async" srcset="./images/webdev.jpg" width="310" height="310" loading="lazy" alt='Expert Web Developer and Designer from Bosnia and Herzegovina'></picture>
+    <h2>About me</h2><p>Hello there!</p><p>I&apos;m Mirnes and I was born in a small town in Bosnia and Herzegovina (Jajce), where I currently lives, although I&apos;ve moved over 30 times over 6 countries in the last 15 years (if you can&apos;t tell, I love to travel!).</p><p>I&apos;m a passionate, dedicated, effective and creative Web Developer, UI/UX and Web Designer with several years of experience in the web development and design industry and with a passion for clean design, standards-based code and open-source software.</p><p>I'm lifelong learner and I'm happiest when I'm creating, learning, exploring and thinking about how to make things better.</p><p>I have skills to convert any idea into a real working app. So let me know your idea, and I would come up with a great plan to make it live.</p><p>In addition to my love of technology and design, I am also interested in education.</p></div></article><article id='about' class='white'><div class='wrapper'>
+<div id='quotes'>
+    <h2>My favorite quotes</h2><p>If you really want to do something you&apos;ll find a way. If you don&apos;t, you&apos;ll find an excuse. - <em>Pablo Picasso</em></p><br class='none'><p>Live as if you were to die tomorrow. Learn as if you were to live forever. ―<em>Mahatma Gandhi</em></p><br class='none'><p>”Choose a job you love, and you will never have to work a day in your life.” -<em>Confucius</em></div></div></article>
+<section id="certificates" aria-label="Certificates by ITAcademy">
 
-    <?php echo $result->render(); ?>
-
-    <section id="certificates" aria-label="Certificates by ITAcademy">
-
-        <div class="wrapper">
-
-            <header>
-
-            <?php
-
-				$ITA = new ITA();
-
-				$ITA->renderSlider();
-
-			?>
-
-            </header>
-
-        </div>
-
-    </section>
+    <div class="wrapper">
+        <header>
+            <h2>Certificates</h2>  
+                <?php
+                    $ITA = new ITA();
+                    $ITA->renderSlider();
+                ?>
+            <article id='mycert'><br class='none'><p>So, finally, in 2016, I decided to enroll in ITAcademy, a school for a profitable IT career in department of PHP WEB DEVELOPMENT.</p><br><p>After successfully completing that program, I was very satisfied with the education provided, and I decided to continue my education in the department of WEB DESIGN.</p><br><p>I enjoy coding and the challenge of learning something new everyday, so over time, I wanted to learn a lot more about the complete web development and design, and continued to educate myself through various web platforms, such as w3Schools, Pluralsight, Eduonix, Udemy, Educba, Shawn Academy, Cybrary and LinkedIn.</p><br> <p>Through the mentioned web platforms, I received over 1000 certificates and their number is continuously growing.</p><br><p class='no_print'>Below you can find and see some of these certificates.</p>
+            </article>
+        </header>
+    </div>
+</section>
 
     <section id="LinkedIn" aria-label="My certificates by LinkedIn and Pluralsight">
 
@@ -380,7 +327,6 @@
             <h2>Projects</h2>
 
             <div class="menu" aria-label="menu">
-
                 <a id="showall" href="#" aria-label="View all my projects"></a>
 
                 <a class="project" target="1" aria-label="Web sites">Web sites</a>
@@ -675,7 +621,7 @@
 
                                 <h3>Multilingual WordPress website for an Olive Oil company</h3>
 
-                                <p>This project contains a responsive multilingual WordPress website for an olive oil company created in English, Korean, Poland, and Croatian language. It's created using WordPress with Elementor and Polylang plugins.</p>
+                                <p>This project contains a responsive multilingual WordPress website for an Olive Oil company created in English, Korean, Poland, and Croatian language. It's created using WordPress with Elementor and Polylang plugins.</p>
 
                                 <p>Also, contains some other plugins such as Contact Form 7 (manage flexible contact forms), Multilingual Contact Form 7 with Polylang (Enables translation and use of the same forms in different languages of Contact Form 7 forms with Polylang), Yoast SEO (helps website perform better in search engines like Google) and WP-Optimize (cleans the database, compresses images and caches pages).</p>
 
@@ -1126,40 +1072,10 @@
 
                 <div id="div4" class="target">
                     <section class='logos'>
-
-                        <article class='logo'><a href='./logos/BIG/cubes.jpg' data-lightbox='logos'><img src='./logos/SMALL/cubes.jpg' alt="Illustration of dynamic cubes by Mirnes Glamočić" class="blur" loading="lazy"><div class="content"><p>Illustration of dynamic cubes created using the Line Tool, Transform, and Outer Glow Effects.</p></div></a>
-
+                        <article class='logo'><a href='./logos/BIG/samsung.jpg' data-lightbox='logos'><img src='./logos/SMALL/samsung.jpg' alt="Samsung logo by Mirnes Glamočić" class="blur" loading="lazy"><div class="content"><p>Samsung logo created using the Ellipse and Type tools, along with the Pathfinder options.</p></div></a>
                         </article><!--
-
-                        --><article class='logo'><a href='./logos/BIG/3D-Star.jpg' data-lightbox='logos'><img src='./logos/SMALL/3D-Star.jpg' alt="Illustration of 3D Star by Mirnes Glamočić" class="blur" loading="lazy"><div class="content"><p>Illustration of a five-pointed 3D twisted woven metal star created using the Ellipse, Rectangle, Line, Rotate, and Scale Tools.</p></div></a>
-
-                        </article><!--
-                        --><article class='logo'><a href='./logos/BIG/ribbon.jpg' data-lightbox='logos'><img src='./logos/SMALL/ribbon.jpg' alt="Illustration of Möbius strip by Mirnes Glamočić" class="blur" loading="lazy"><div class="content"><p>Illustration of an overlapping ribbon created by a combination of hexagons, a blend of objects, the Rotate Tool, and the Live Paint Bucket.</p></div></a>
-
-                        </article><!--
-
-                        --><article class='logo'><a href='./logos/BIG/orange.jpg' data-lightbox='logos'><img src='./logos/SMALL/orange.jpg' alt="Illustration of orange wedge by Mirnes Glamočić" class="blur" loading="lazy"><div class="content"><p>Illustration of a single orange slice created using the Ellipse and Rectangle Tool with the Scale and Rotate Tool.</p></div></a>
-
-                        </article><!--
-
-                        --><article class='logo'><a href='./logos/BIG/flower_petals.jpg' data-lightbox='logos'><img src='./logos/SMALL/flower_petals.jpg' alt="Illustration of flower petals by Mirnes Glamočić" class="blur" loading="lazy"><div class="content"><p>Illustration of flower petals created using Ellipse Tool, Transform Effects, Pucker & Bloat, and Zig Zag.</p></div></a>
-
-                        </article><!--
-
-                        --><article class='logo'><a href='./logos/BIG/electron.jpg' data-lightbox='logos'><img src='./logos/SMALL/electron.jpg' alt="Illustration of the atomic symbol by Mirnes Glamočić" class="blur" loading="lazy"><div class="content fade"><p>Illustration of the atomic symbol created using the Ellipse Tool with the help of the Effects from the Appearance panel.</p></div></a>
-
-                        </article><!--
-
-                        --><article class='logo'>
-
-                        <a href='./logos/BIG/ellipses.jpg' data-lightbox='logos'><img src='./logos/SMALL/ellipses.jpg' alt="Illustration of multiple ellipses by Mirnes Glamočić" class="blur" loading="lazy"><div class="content fade"><p>Illustration of multiple ellipses created using the Ellipse and Rectangle Tool with the help of the Effects from the Appearance panel.</p></div></a></article><!--
-
-                        --><article class='logo'><a href='./logos/BIG/honeycomb.jpg' data-lightbox='logos'><img src='./logos/SMALL/honeycomb.jpg' alt="Illustration of Sheriff's badge by Mirnes Glamočić" class="blur" loading="lazy"><div class="content"><p>Illustration of honeycomb created with Polygon Tool using a combination of Effects from Appearance panel.</p></div></a>
-
-                        </article><!--
-
-                        --><article class='logo'>
-                            <a href='./logos/BIG/pixelworker-star.jpg' data-lightbox='logos'><img src='./logos/SMALL/pixelworker-star.jpg' alt="Star logo of United Pixelworkers by Mirnes Glamočić" class="blur" loading="lazy"><div class="content fade"><p>Star logo by United Pixelworkers created using Ellipse, Rotate, Scale, and Shear tools with warped gradient shadows.</p></div></a>
+                            
+                        --><article class='logo'><a href='./logos/BIG/pixelworker-star.jpg' data-lightbox='logos'><img src='./logos/SMALL/pixelworker-star.jpg' alt="Star logo of United Pixelworkers by Mirnes Glamočić" class="blur" loading="lazy"><div class="content fade"><p>Star logo by United Pixelworkers created using Ellipse, Rotate, Scale, and Shear tools with warped gradient shadows.</p></div></a>
                         </article><!--
 
                         --><article class='logo'><a href='./logos/BIG/complementary-rings.jpg' data-lightbox='logos'><img src='./logos/SMALL/complementary-rings.jpg' alt="Complemtary ring logo by Mirnes Glamočić" class="blur" loading="lazy"><div class="content"><p>Complemtary ring logo, created from a interwoven ring logo stitched together using the Ellipse, Rotate, and Scissors tools, along with the paths join command.</p></div></a>
@@ -1280,7 +1196,12 @@
     </section> <!--contact-->
 <!-- Placed at the end of the document so the pages load faster -->
 <script>
-   jQuery(document).ready(function(){jQuery(function(){jQuery("#showall").click(function(){jQuery(".target").show()}),jQuery(".project").click(function(){jQuery(".target").hide(),jQuery("#div"+jQuery(this).attr("target")).show()})})});
+(function($){
+    document.getElementById("div1").style.display = "block";
+})(jQuery);
+    jQuery(document).ready(function(){
+    jQuery(function(){jQuery("#showall").click(function(){
+    jQuery(".target").show()}),jQuery(".project").click(function(){jQuery(".target").hide(),jQuery("#div"+jQuery(this).attr("target")).show()})})});
 </script> 
 
 <?php
