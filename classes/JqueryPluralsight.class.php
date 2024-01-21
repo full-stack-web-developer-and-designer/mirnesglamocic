9cbe0c6a660c9aa354d149bd9476aac11617720a
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-01-21 10:59:28 +0100

    Reanmed file

--- classes/JqueryPluralsight.class.php
+++ classes/JqueryPluralsight.class.php
@@ -1,5 +1,5 @@
 <?php
-class jQueryPluralsightSlide extends Slider{
+class JqueryPluralsightSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
         $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='jQuery' AND class='Pluralsight preview' ORDER BY rand();";
