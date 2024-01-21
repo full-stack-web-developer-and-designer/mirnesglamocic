deb475ecef7c32a23d793a6df1e0ba5b0b7e7dec
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-01-21 10:50:56 +0100

    Renamed file

--- classes/CssSlide.class.php
+++ classes/CssSlide.class.php
@@ -1,5 +1,5 @@
 <?php
-class CSSSlide extends Slider{
+class CssSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
         $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='CSS' AND class='LinkedIn preview'
