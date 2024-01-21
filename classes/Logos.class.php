4ba43b2869bf37316112c85efa339fd710b2de02
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-01-21 03:40:12 +0100

    Created class for logo projects

---
+++ classes/Logos.class.php
@@ -0,0 +1,17 @@
+<?php
+class Logos extends ConnectSlider{
+     // Fetch data from MySQL using PDO - PHP Data Object	
+	public function renderSlider(){
+        $sql = "SELECT * FROM mirnesgl_cert.projects WHERE article='logo';";
+        $stmt = $this->__connect()->query($sql);
+       
+        while($row = $stmt->fetch()) {
+            $article = $row['article'];
+			$image = $row['image'];
+            $alt = $row['alt'];
+            $content = $row['content'];
+
+            echo "<article class=\"$article\"><a href=\"./logos/BIG/$image\" data-lightbox=\"logos\"><img src=\"./logos/SMALL/$image\" alt=\"$alt by Web Designer and Developer Mirnes Glamočić from Bosnia and Herzegovina\" class='blur' loading='lazy'><div class='content fade'><p>$content</p></div></a></article>";
+        }
+    }
+}
