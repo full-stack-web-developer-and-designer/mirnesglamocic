89cc04a53c07d07849ba2747b0bf3b1b38ff8ec0
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-01-21 10:17:59 +0100

    Added webp prefix for img into class and the same part of alt tag

--- classes/MongoDBSlide.class.php
+++ classes/MongoDBSlide.class.php
@@ -1,5 +1,5 @@
 <?php
-class MongoDBSlide extends Slider{
+class MongoDbSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
         $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='MongoDB' OR skill='NoSQL' ORDER BY rand();";
@@ -11,7 +11,7 @@ class MongoDBSlide extends Slider{
             $class = $row['class'];
             echo "<li class='item-a slide'>
             <article class='cert'>
-            <a href='./cert/BIG/$picture' data-href='./cert/SMALL/$picture' class='progressive replace' data-lightbox='image-group'><img src='./cert/SMALL/$picture' alt=\"$alt\" class=\"$class\" loading=\"lazy\"></a>
+            <a href='./cert/BIG/$picture.webp' data-href='./cert/SMALL/$picture.webp' class='progressive replace' data-lightbox='image-group'><img src='./cert/SMALL/$picture.webp' alt=\"$alt by Web Designer and Developer Mirnes Glamočić from Bosnia and Herzegovina\" class=\"$class\" loading=\"lazy\"></a>
             </article></li>";
             }
         }
