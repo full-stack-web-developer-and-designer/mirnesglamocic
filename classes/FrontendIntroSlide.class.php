14aaa9309b411c22ed19530070cd34e73017e0ac
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-01-21 23:50:41 +0100

    Included certificates FrontendIntro WebDesign into frontend certifications

--- classes/FrontendIntroSlide.class.php
+++ classes/FrontendIntroSlide.class.php
@@ -2,7 +2,7 @@
 class FrontendIntroSlide extends Slider{
     // Fetch data from MySQL using PDO - PHP Data Object	
 	public function renderSlider(){
-        $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='FrontendIntro' OR skill='FrontendIntro UX WebDesign' ORDER BY rand();";
+        $sql = "SELECT * FROM mirnesgl_cert.certifications WHERE skill='FrontendIntro' OR skill='FrontendIntro WebDesign'  OR skill='FrontendIntro UX WebDesign' ORDER BY rand();";
         $stmt = $this->__connect()->query($sql);
 
         while($row = $stmt->fetch()) {
