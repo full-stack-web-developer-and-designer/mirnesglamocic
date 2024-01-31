92c616bae329f434edddb4585b1ad05b8f300194
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-01-31 02:31:39 +0100

    Changed min-width for images

--- classes/ITA.class.php
+++ classes/ITA.class.php
@@ -19,15 +19,15 @@ class ITA extends ConnectSlider{
 
             echo "<article id=\"$article\">
                     <a href=\"./images/BIG/$href\" data-lightbox=\"ita\">
-                        <picture>
+                        <picture class='ita'>
                             <!--[if IE 9]><video style='display: none;'><![endif]-->
                             <source srcset=\"$img_400\" type=\"image/webp\" width=\"400\" height=\"565\" loading=\"lazy\" media=\"(min-width: 1261px)\">
-                            <source srcset=\"$img_300\" type=\"image/webp\" width=\"300\" height=\"424\" loading=\"lazy\" media=\"(min-width: 1023px)\">
-                            <source srcset=\"$img_250\" type=\"image/webp\" width=\"250\" height=\"353\" loading=\"lazy\" media=\"(min-width: 564px)\">
-                            <source srcset=\"$img_200\" type=\"image/webp\" width=\"200\" height=\"283\" loading=\"lazy\" media=\"(min-width: 453px)\">
-                            <source srcset=\"$img_180\" type=\"image/webp\" width=\"180\" height=\"254\" loading=\"lazy\" media=\"(min-width: 414px)\">
-                            <source srcset=\"$img_170\" type=\"image/webp\" width=\"170\" height=\"240\" loading=\"lazy\" media=\"(min-width: 390px)\">
-                            <source srcset=\"$img_300\" type=\"image/webp\" width=\"300\" height=\"424\" loading=\"lazy\" media=\"(max-width: 389px)\">
+                            <source srcset=\"$img_300\" type=\"image/webp\" width=\"300\" height=\"424\" loading=\"lazy\" media=\"(min-width: 1024px)\">
+                            <source srcset=\"$img_250\" type=\"image/webp\" width=\"250\" height=\"353\" loading=\"lazy\" media=\"(min-width: 768px)\">
+                            <source srcset=\"$img_200\" type=\"image/webp\" width=\"200\" height=\"283\" loading=\"lazy\" media=\"(min-width: 521px)\">
+                            <source srcset=\"$img_180\" type=\"image/webp\" width=\"180\" height=\"254\" loading=\"lazy\" media=\"(min-width: 430px)\">
+                            <source srcset=\"$img_170\" type=\"image/webp\" width=\"170\" height=\"240\" loading=\"lazy\" media=\"(min-width: 389px)\">
+                            <source srcset=\"$img_300\" type=\"image/webp\" width=\"300\" height=\"424\" loading=\"lazy\" media=\"(max-width: 388px)\">
                             <!--[if IE 9]></video><![endif]-->
                             <img decoding=\"async\" srcset=\"$img\" width=\"400\" height=\"565\" loading=\"lazy\" alt=\"$alt\">
                         </picture></a>
