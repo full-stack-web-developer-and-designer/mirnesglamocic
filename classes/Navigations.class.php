0c79e2018c4fd7d35a63bd7d0dec50500799184e
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-01-31 02:21:41 +0100

    Changed link to projects

--- classes/Navigations.class.php
+++ classes/Navigations.class.php
@@ -7,7 +7,7 @@ class Navigations extends Entity{
 		$render = "<li><a href=\"/#aboutme\" aria-label=\"about me\">$this->about</a><li> ";
         $render .= "<li><a href=\"/#certificates\" aria-label=\"my certificates\">$this->certificates</a></li>";
         $render .= "<li><a href=\"/#skills\" aria-label=\"my skills\">$this->skills</a></li>";
-        $render .= "<li><a href=\"/#projects\" aria-label=\"my projects\">$this->projects</a></li>";
+        $render .= "<li><a href=\"/projects.html\" aria-label=\"my projects\">$this->projects</a></li>";
         $render .= "<li><a href=\"/#services\" aria-label=\"my services\">$this->services</a></li>";
         $render .= "<li><a href=\"/#contact\" aria-label=\"contact me\">$this->contact</a></li>";
 		return $render;
