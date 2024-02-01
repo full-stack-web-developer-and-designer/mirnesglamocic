08da4b66d2e48fe51b3f42d484166f109a4befcb
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-02-01 04:53:51 +0100

    Added rel canonical tag

--- webdesign.php
+++ webdesign.php
@@ -4,16 +4,16 @@ require_once 'core/init.php';
 <!DOCTYPE html>
 <html lang="en">
 <?php
-	$fb_title="Web Design Certificates by Mirnes Glamočić";
-	$meta['og_description']="Mirnes Glamočić is an experienced Web Designer with several years of experience in the web development and design industry.";
+	$fb_title="Web Design Certificates of IT expert Mirnes Glamočić";
+	$meta['og_description']="See the Web Design certificates of professional web designer and developer Mirnes Glamočić from Bosnia and Herzegovina.";
+	$title=$fb_title;
+	$meta['description']=$meta['og_description'] ." The best of the best!";
 	$og_url="https://mirnesglamocic.com/webdesigncertificates.html";
 	$og_image="https://mirnesglamocic.com/images/webdesign_og.jpg";
 	$og_image_alt=$fb_title;
 	$linkedin_title=$fb_title;
-	$title=$fb_title;
-	$meta['description']="Mirnes Glamočić is an experienced Frontend Developer with several years of experience in the web development industry. Also, he is experienced in full-stack web development and design.";
 	$twitter_image="https://mirnesglamocic.com/images/webdesign_og.webp";
-	$meta['keywords']="web development, web developer, front-end development, frontend development, front-end developer, frontend developer, web design, web designer, ui design, ux design, ui/ux design";
+	$canonical = "https://mirnesglamocic.com/webdesigncertificates.html";
 	include './inc/head.php';
 ?>
 <body class="development design">
