dedeb8f5a9283a7ab101b207c5a4229fcfcc3688
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-02-03 06:40:11 +0100

    Removed role='link' and added href and name attributes for scroll top

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,4 @@
-<a role="link" tabindex="0" aria-label="Scroll to top" class="scroll-top"><i class="fa fa-arrow-up"></i></a>
+<a href="#" name="Scroll-to-top" aria-label="Scroll to top" class="scroll-top"><i class="fa fa-arrow-up"></i></a>
 <?php $url = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>
 <footer id="footer">
 <?php
