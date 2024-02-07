2ac24b2403e519988ce05038b4ee1ea3151fb715
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-02-07 03:00:32 +0100

    Changed fontawesome icons to SVG

--- inc/head.php
+++ inc/head.php
@@ -1,16 +1,14 @@
 <!--
 Wow! Welcome to my code. If you need a professional website don't hesitate to contact me: https://mirnesglamocic.com
 -->
-<head id="head">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php echo $title; ?></title>
-<!-- meta tags -->
-<meta name="robots" content="index,follow">
 <meta name="description" content="<?php echo $meta['description']; ?>">
-<meta name="theme-color" content="#0648a0">
 <meta name="author" content="Mirnes Glamočić">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="robots" content="index,follow">
+<meta name="theme-color" content="#273142;">
 <!-- Facebook & Viber -->
 <meta name="title" property="og:title" content="<?php echo $title;?>">
 <!-- Facebook, Viber, LinkedIn, Whatsapp & KakaoTalk(meta tags) -->
@@ -51,14 +49,20 @@ Wow! Welcome to my code. If you need a professional website don't hesitate to co
 <link rel="android-chrome-icon" sizes="192x192" href="./android-chrome-192x192.png">
 <link rel="canonical" href="<?php echo $og_url;?>">
 <!-- Custom CSS -->
-<link rel="stylesheet" href="./CSS/style.min.css" media="all">
-<link rel="stylesheet" href="./CSS/<?php echo $css;?>" media="all">
+<link rel="stylesheet" href="/CSS/style.min.css" media="all">
+<link rel="stylesheet" href="/CSS/<?php echo $css;?>" media="all">
 <!-- lightslider CSS-->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/css/lightslider.min.css" rel="stylesheet">
 <!-- lightbox css -->
-<link rel="stylesheet" type="text/css" href="./lightbox2/src/css/lightbox.min.css">
+<link rel="stylesheet" type="text/css" href="/lightbox2/src/css/lightbox.min.css">
 <!-- include fontawesome  -->
 <link rel="preload" as="style" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" onload="this.rel='stylesheet'" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+<noscript>
+    <style type="text/css">
+    .cert { display:none;}
+    </style>
+    To display the page correctly, please enable JavaScript in your browser!
+</noscript>
 <!-- include fontawesome if cdn fails -->
 <script src="https://kit.fontawesome.com/c3fb2ab6eb.js" crossorigin="anonymous"></script>
 <!-- page reload if the user changes mobile orientation -->
@@ -67,16 +71,17 @@ Wow! Welcome to my code. If you need a professional website don't hesitate to co
 </script>
 <!-- jQuery -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<!-- include jQuery if cdn fails-->
 <script>
 	window.jQuery || document.write("<script src='./JS/jQuery-1.7.2.min.js'><\/script>");
 </script>
-<noscript><p class="errorJS">To display the page correctly, please enable JavaScript in your browser!</p></noscript>
 <!-- picturefill -->
 <script>
     // Picture element HTML5 shiv
     document.createElement( "picture" );
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/picturefill/3.0.2/picturefill.min.js" integrity="sha512-Nh/FthCqOFq56kGp1DsNb6GKK29iIv2ZJc7Fylln1WRrWpeE5LjSBWeAp61dkBASDxZT12aL9mZyIrhsAqgRpw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- include picturefill if cdn fails-->
 <script>
 	window.jQuery || document.write("<script src='./JS/picturefill.min.js'><\/script>");
 </script>
@@ -88,4 +93,3 @@ Wow! Welcome to my code. If you need a professional website don't hesitate to co
     var webpMachine = new webpHero.WebpMachine();
     webpMachine.polyfillDocument();
 </script>
-</head>
