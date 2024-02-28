dd0637f40d4cc0c825501f848557e42410479b18
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-02-28 21:28:05 +0100

    Removed name attribute for anchors

--- projects.php
+++ projects.php
@@ -2,8 +2,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-	$title = "Portfolio of web design and development projects by Mirnes Glamočić";
-	$meta['description'] = "View a portfolio of web design and development projects by professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina. The best of the best!";
+	$title = "Portfolio of web design and web development projects by Mirnes Glamočić";
+	$meta['description'] = "View a portfolio of web design and web development projects by professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina. The best of the best!";
 	$og_url = "https://mirnesglamocic.com/projects.html";
 	$og_image = "https://mirnesglamocic.com/images/Mirnes_og.jpg";
 	$twitter_image = "https://mirnesglamocic.com/images/MirnesGlamocic.webp";
@@ -13,14 +13,14 @@
 <body>
 <?php include 'inc/header.php'; ?>
     <div class="wrapper">
-        <section id="projects" aria-label="Web design and development projects by professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+        <section id="projects" aria-label="Web design and web development projects by professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
             <h1>Projects</h1>
                 <div class="menu" aria-label="menu">
                     <a id="showall" href="./projects.html" aria-label="View all my projects"></a>
-                    <a name="websites" class="project" target="1" aria-label="Web sites"><h2>Web sites</h2></a>
-                    <a name="photoshops" class="project" target="2" aria-label="Projects of edited images by web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina"><h2>Image editing</h2></a> 
-                    <a name="illustrations" class="project" target="3" aria-label="Illustration projects by professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina"><h2>Illustrations</h2></a> 
-                    <a name="logos" class="project" target="4" aria-label="Logo projects by professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina"><h2>Logo design</h2></a> 
+                    <a class="project" target="1" aria-label="Web sites"><h2>Web sites</h2></a>
+                    <a class="project" target="2" aria-label="Projects of edited images by web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina"><h2>Image editing</h2></a> 
+                    <a class="project" target="3" aria-label="Illustration projects by professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina"><h2>Illustrations</h2></a> 
+                    <a class="project" target="4" aria-label="Logo projects by professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina"><h2>Logo design</h2></a> 
                 </div>
                 <!-- websites -->
                 <div class="taget_box">
@@ -64,12 +64,7 @@
 <?php include_once './inc/footer.php';//end #footer ?>
 <!-- Placed at the end of the document so the pages load faster -->
 <script>
-    (function($){
-        document.getElementById("div1").style.display = "block";
-    })(jQuery);
-        jQuery(document).ready(function(){
-        jQuery(function(){jQuery("#showall").click(function(){
-        jQuery(".target").show()}),jQuery(".project").click(function(){jQuery(".target").hide(),jQuery("#div"+jQuery(this).attr("target")).show()})})});
+    jQuery("#div1").css("display","block"),jQuery(document).ready(function(){jQuery(function(){jQuery("#showall").click(function(){jQuery(".target").show()}),jQuery(".project").click(function(){jQuery(".target").hide(),jQuery("#div"+jQuery(this).attr("target")).show()})})});
 </script>
 </body>
 </html>
