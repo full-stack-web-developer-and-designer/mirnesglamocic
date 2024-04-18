c7b492bea2bce4a278c12b8e3dd8126b19505f44
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-04-18 23:39:17 +0900

    Changed PHP variables for show content of pages

--- classes/Page.class.php
+++ classes/Page.class.php
@@ -1,15 +1,15 @@
 <?php
 class Page{
 	private static $db;
-	public static function get($id){
-		$q = self::$db->query("SELECT * FROM pages WHERE page_id = {$id}");
-		$post = $q->fetchObject('Page');
-		return $post;
+	public static function get($page_id){
+		$sql = self::$db->query("SELECT * FROM pages WHERE page_id = {$page_id}");
+		$page = $sql->fetchObject('Page');
+		return $page;
 	}
 	public static function getAll(){
-		$q = self::$db->query("SELECT * FROM pages");
-		$postArr = $q->fetchAll();
-		return $postArr;
+		$sql = self::$db->query("SELECT * FROM pages");
+		$pageArr = $sql->fetchAll();
+		return $pageArr;
 	}	
 	public static function init(){
 		self::$db = Connect::getInstance();
