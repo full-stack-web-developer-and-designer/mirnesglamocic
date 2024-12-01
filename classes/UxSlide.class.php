429f98b8bb29e8c169e3a77ae25662f251990fb0
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-12-01 07:52:50 +0100

    Added UX certificates

---
+++ classes/UxSlide.class.php
@@ -0,0 +1,18 @@
+<?php
+class UxSlide extends Slider{
+    // Fetch data from MySQL using PDO - PHP Data Object	
+	public function renderSlider(){
+        $sql = "SELECT * FROM mirnesgl_korea.certifications WHERE skill='UX' OR skill='Photoshop UX' OR skill='UX Web Design' ORDER BY rand();";
+        $stmt = $this->__connect()->query($sql);
+
+        while($row = $stmt->fetch()) {
+        	$picture = $row['picture'];
+			$alt = $row['alt'];
+            $class = $row['class'];
+            echo "<li class='item-a slide'>
+            <article class='cert' aria-label='UX certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina'>
+            <a href='./cert/BIG/$picture.webp' data-href='./cert/SMALL/$picture.webp' data-lightbox='image-group'><img src='./cert/SMALL/$picture.webp' alt=\"$alt by Web Designer and Developer Mirnes Glamočić from Bosnia and Herzegovina\" class=\"$class\" loading=\"lazy\"></a>
+            </article></li>";
+            }
+        }
+    }
