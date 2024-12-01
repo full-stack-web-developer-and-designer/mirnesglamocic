3e59cbfd4bb915f7064ef3574f1c06205bb5f10d
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-12-01 19:14:11 +0100

    Created class for display pages

---
+++ classes/Page.class.php
@@ -0,0 +1,18 @@
+<?php
+class Page{
+	private static $db;
+	public static function get($page_id){
+		$sql = self::$db->query("SELECT * FROM pages WHERE page_id = {$page_id}");
+		$page = $sql->fetchObject('Page');
+		return $page;
+	}
+	public static function getAll(){
+		$sql = self::$db->query("SELECT * FROM pages");
+		$pageArr = $sql->fetchAll();
+		return $pageArr;
+	}	
+	public static function init(){
+		self::$db = Connect::getInstance();
+	}
+}
+	
---
+++ classes/Pages.class.php
@@ -0,0 +1,10 @@
+<?php
+class Pages extends Entity{
+	public static $tableName = 'pages';
+	public static $keyColumn = 'page_id';
+	
+	public function render(){
+		$render = "<h1>$this->title</h1>";
+		return $render;
+	}
+}
