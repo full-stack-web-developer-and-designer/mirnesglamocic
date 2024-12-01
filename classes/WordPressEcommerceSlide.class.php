0182aacc5e234f49d6c07c63f7fe61fdbf079dc0
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-12-01 07:48:55 +0100

    Added WordPress certificates

---
+++ classes/WordPressEcommerceSlide.class.php
@@ -0,0 +1,18 @@
+<?php
+class WordPressEcommerceSlide extends Slider{
+    // Fetch data from MySQL using PDO - PHP Data Object	
+	public function renderSlider(){
+        $sql = "SELECT * FROM mirnesgl_korea.certifications WHERE skill='WordPressEcommerce' ORDER BY rand();";
+        $stmt = $this->__connect()->query($sql);
+
+        while($row = $stmt->fetch()) {
+        	$picture = $row['picture'];
+			$alt = $row['alt'];
+            $class = $row['class'];
+            echo "<li class='item-a slide'>
+            <article class='cert' aria-label='WordPress certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina'>
+            <a href='./cert/BIG/$picture.webp' data-href='./cert/SMALL/$picture.webp' class='progressive replace' data-lightbox='image-group'><img src='./cert/SMALL/$picture.webp' alt=\"$alt by Web Designer and Developer Mirnes Glamočić from Bosnia and Herzegovina\" class=\"$class\" loading=\"lazy\"></a>
+            </article></li>";
+            }
+        }
+    }
