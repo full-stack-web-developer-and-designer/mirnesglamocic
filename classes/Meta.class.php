bc5cebed98e3a0d21091e78a05d21630a1e093ec
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-12-01 19:12:56 +0100

    Created class for Meta tags

---
+++ classes/Meta.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class Meta extends ConnectSlider{
+    // Fetch data from MySQL using PDO - PHP Data Object	
+	public function render(){
+		$question = "SELECT * FROM visitneu_neum.meta;";
+		$result=$this->__connection()->query($question) or die("Error: $query");
+		
+		while ($row = $result->fetch()) {
+		$title = $row['title'];
+		
+		echo "head id=\"head\">
+		<title> $title; </title>";
+		
+		}
+	}
+}
