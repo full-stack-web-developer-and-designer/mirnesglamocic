be6d7d91a6a059d001bd8834fa58801ad69ea387
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-12-01 19:05:57 +0100

    Created Class Entity for DB

---
+++ classes/Entity.class.php
@@ -0,0 +1,29 @@
+<?php
+// Active record pattern
+class Entity{
+	private static $db;
+	// Remove Warning Deprecated
+	public $page_id;
+	public $meta_id;
+    public $title;
+    public $description;
+	public static function get($id){
+		$tableName = static::$tableName;
+		$keyColumn = static::$keyColumn;
+		$className = get_called_class();
+		$q = "SELECT * FROM {$tableName} WHERE {$keyColumn} = {$id}";
+		$q = self::$db->query($q);
+		$result = $q->fetchObject($className);
+		return $result;
+	}
+	public static function getAll(){
+		$tableName = static::$tableName;
+		$q = self::$db->query("SELECT * FROM {$tableName}");
+		$postArr = $q->fetchAll();
+		return $postArr;
+	}	
+	public static function init(){
+		self::$db = DB::getInstance();
+	}
+}
+Entity::init();
