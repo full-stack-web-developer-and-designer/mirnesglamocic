d92a38d6863ede0c1a0a9303572b49d2f0031506
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-12-01 19:15:15 +0100

    Created class for diplsay contact icons

---
+++ classes/Icons.class.php
@@ -0,0 +1,18 @@
+<?php
+class Icons extends ConnectSlider{
+     // Fetch data from MySQL using PDO - PHP Data Object	
+	public function renderSlider(){
+        $sql = "SELECT * FROM mirnesgl_korea.contact_icon";
+        $stmt = $this->__connect()->query($sql);
+       
+        while($row = $stmt->fetch()) {
+            $href = $row['href'];
+			$aria_label = $row['aria_label'];
+            $svg = $row['svg'];
+
+            echo "<span class='icon_box'>
+            <a class='social-icon' href=\"$href\" aria-label=\"$aria_label\" target='_blank'>$svg</a>   
+            </span>";
+        }
+    }
+}
