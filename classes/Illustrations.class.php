fac692ca1e8b16e4a8472388aa8c0e7b0a58e6b9
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-12-01 19:19:19 +0100

    Created class for Illustration Projects

---
+++ classes/Illustrations.class.php
@@ -0,0 +1,17 @@
+<?php
+class Illustrations extends ConnectSlider{
+     // Fetch data from MySQL using PDO - PHP Data Object	
+	public function renderSlider(){
+        $sql = "SELECT * FROM mirnesgl_korea.projects WHERE article='illustration' ORDER BY projects_id DESC;";
+        $stmt = $this->__connect()->query($sql);
+       
+        while($row = $stmt->fetch()) {
+            $article = $row['article'];
+			$image = $row['image'];
+            $alt = $row['alt'];
+            $content = $row['content'];
+
+            echo "<article class=\"$article\" aria-label='Projects of illustrations by professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina'><a href=\"./illustrations/BIG/$image.jpg\" data-lightbox='illustrations'><img src=\"./illustrations/SMALL/$image.jpg\" alt=\"$alt by professional web designer and programmer Mirnes Glamočić from Bosnia and Herzegovina\" class='blur' loading='lazy'><div class='content fade'><p>$content</p></div></a></article>";
+        }
+    }
+}
