a76ad1da28700973ee51f2ff7108f102cf285348
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-12-01 18:38:22 +0100

    Added Frontend Certificates

---
+++ classes/FrontendIntroSlide.class.php
@@ -0,0 +1,18 @@
+<?php
+class FrontendIntroSlide extends Slider{
+    // Fetch data from MySQL using PDO - PHP Data Object	
+	public function renderSlider(){
+        $sql = "SELECT * FROM mirnesgl_korea.certifications WHERE skill='FrontendIntro' OR skill='FrontendIntro WebDesign'  OR skill='FrontendIntro UX WebDesign' ORDER BY rand();";
+        $stmt = $this->__connect()->query($sql);
+
+        while($row = $stmt->fetch()) {
+        	$picture = $row['picture'];
+			$alt = $row['alt'];
+            $class = $row['class'];
+            echo "<li class='item-a slide'>
+            <article class='cert' aria-label='Front-end certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina'>
+            <a href='./cert/BIG/$picture.webp' data-href='./cert/SMALL/$picture.webp' class='progressive replace' data-lightbox='image-group'><img src='./cert/SMALL/$picture.webp' alt=\"$alt by Web Designer and Developer Mirnes Glamočić from Bosnia and Herzegovina\" class=\"$class\" loading=\"lazy\"></a>
+            </article></li>";
+            }
+        }
+    }
