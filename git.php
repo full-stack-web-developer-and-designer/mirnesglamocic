64206f2a41d66accfe5cc1ba327c616b719d3a47
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2024-12-04 23:10:38 +0100

    Changed variable names to lowercase and lowerCamelCase

--- git.php
+++ git.php
@@ -1,52 +1,45 @@
 <?php
-	require_once 'core/init.php';
-	$head = Head::get(11);
-    $page_id=11;
+    require_once 'core/init.php';
+    $head = Head::get(11);
+    $page_id = 11;
     $page = Pages::get(11);
 ?>
 <!DOCTYPE html>
 <html itemscope lang="en">
-	<?php
-		echo $head->render();
-	?>
-	<body>
-		<?php include 'inc/header.php';?>
-		<div class="wrapper">
-
-			<?php echo $page->render(); ?>
-		
-			<section class="slider-container" aria-label="GIT certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$Git = new GitSlide();
-						$Git->renderSlider();
-						$Vcs = new VcsSlide();
-						$Vcs->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="GitHub certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$GitHubNew = new GitHubNewSlide();
-						$GitHubNew->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="GitHub certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$GitHub = new GitHubSlide();
-						$GitHub->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-		</div><!--end .wrapper-->
-		<?php include_once './inc/footer.php';//end #footer?>
-	</body>
+    <?php echo $head->render(); ?>
+    <body>
+        <?php include 'inc/header.php'; ?>
+        <div class="wrapper">
+            <?php echo $page->render(); ?>
+            <section class="slider-container" aria-label="GIT certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $git = new GitSlide();
+                        $git->renderSlider();
+                        $vcs = new VcsSlide();
+                        $vcs->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="GitHub certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $gitHubNew = new GitHubNewSlide();
+                        $gitHubNew->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="GitHub certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $gitHub = new GitHubSlide();
+                        $gitHub->renderSlider();
+                    ?>
+                </ul>
+            </section>
+        </div>
+        <?php include_once './inc/footer.php';//end #footer ?>
+    </body>
 </html>
