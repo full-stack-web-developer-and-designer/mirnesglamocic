7b2fb4a1221a664f83e32ae1da9697836420f68d
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2024-12-04 23:06:42 +0100

    Changed variable names to lowercase and lowerCamelCase

--- frontend.php
+++ frontend.php
@@ -1,171 +1,152 @@
-<?php
-	require_once 'core/init.php';
-	$head = Head::get(2);
-    $page_id=2;
+<?php 
+    require_once 'core/init.php';
+    $head = Head::get(2);
+    $page_id = 2;
     $page = Pages::get(2);
 ?>
 <!DOCTYPE html>
 <html itemscope lang="en">
-	<?php
-		echo $head->render();
-	?>
-	<body>
-		<?php include 'inc/header.php'; ?>
-		<div class="wrapper">
-			
-			<?php echo $page->render(); ?>
+    <?php echo $head->render(); ?>
+    <body>
+        <?php include 'inc/header.php'; ?>
+        <div class="wrapper">
+            <?php echo $page->render(); ?>
 
-			<section class="slider-container" aria-label="Front-end certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$FrontendSoloLearn = new FrontendSoloLearnSlide();
-						$FrontendSoloLearn->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-			
-			<section class="slider-container" aria-label="Front-end certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$Frontend = new FrontendSlide();
-						$Frontend->renderSlider();
-						$FrontendIntro = new FrontendIntroSlide();
-						$FrontendIntro->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="HTML certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$Html = new HtmlSlide();
-						$Html->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-			
-			<section class="slider-container" aria-label="HTML and CSS certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$HtmlCss = new HtmlCssSlide();
-						$HtmlCss->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-			
-			<section class="slider-container" aria-label="CSS certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$CssNew = new CssNewSlide();
-						$CssNew->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-			<section class="slider-container" aria-label="CSS certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$Css = new CssSlide();
-						$Css->renderSlider();
-						$Sass = new SassSlide();
-						$Sass->renderSlider();
-						$Tailwind = new TailwindSlide();
-						$Tailwind->renderSlider();
-
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="Bootstrap certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$Bootstrap = new BootstrapSlide();
-						$Bootstrap->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="Web Development certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$WebDevelopment = new WebDevelopmentSlide();
-						$WebDevelopment->renderSlider();
-						$ViewSource = new ViewSourceSlide();
-						$ViewSource->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="JavaScript certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$JavaScript = new JavaScriptSlide();
-						$JavaScript->renderSlider();
-						$TypeScript = new TypeScriptSlide();
-						$TypeScript->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="JavaScript certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$JavaScriptPluralsight = new JavaScriptPluralsightSlide();
-						$JavaScriptPluralsight->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-			
-			<section class="slider-container" aria-label="jQuery certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$Jquery = new JquerySlide();
-						$Jquery->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="jQuery certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$JqueryPluralsight = new JqueryPluralsightSlide();
-						$JqueryPluralsight->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="Angular certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$Angular = new AngularSlide();
-						$Angular->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="React certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$React = new ReactSlide();
-						$React->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-			
-		</div><!--end .wrapper-->
-		<?php include_once './inc/footer.php';//end #footer?>
-	</body>
+            <section class="slider-container" aria-label="Front-end certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $frontendSoloLearn = new FrontendSoloLearnSlide();
+                        $frontendSoloLearn->renderSlider(); 
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="Front-end certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $frontend = new FrontendSlide();
+                        $frontend->renderSlider();
+                        $frontendIntro = new FrontendIntroSlide();
+                        $frontendIntro->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="HTML certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $html = new HtmlSlide();
+                        $html->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="HTML and CSS certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $htmlCss = new HtmlCssSlide();
+                        $htmlCss->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="CSS certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $cssNew = new CssNewSlide();
+                        $cssNew->renderSlider(); 
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="CSS certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $css = new CssSlide();
+                        $css->renderSlider();
+                        $sass = new SassSlide();
+                        $sass->renderSlider();
+                        $tailwind = new TailwindSlide();
+                        $tailwind->renderSlider(); ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="Bootstrap certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $bootstrap = new BootstrapSlide();
+                        $bootstrap->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="Web Development certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php 
+                        $webDevelopment = new WebDevelopmentSlide();
+                        $webDevelopment->renderSlider();
+                        $viewSource = new ViewSourceSlide();
+                        $viewSource->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="JavaScript certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $javaScript = new JavaScriptSlide();
+                        $javaScript->renderSlider();
+                        $typeScript = new TypeScriptSlide();
+                        $typeScript->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="JavaScript certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php 
+                        $javaScriptPluralsight = new JavaScriptPluralsightSlide();
+                        $javaScriptPluralsight->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="jQuery certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php 
+                        $jQuery = new JquerySlide();
+                        $jQuery->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="jQuery certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $jQueryPluralsight = new JqueryPluralsightSlide();
+                        $jQueryPluralsight->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="Angular certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $angular = new AngularSlide();
+                        $angular->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="React certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $react = new ReactSlide();
+                        $react->renderSlider();
+                    ?>
+                </ul>
+            </section>
+        </div>
+        <?php include_once './inc/footer.php';//end #footer ?>
+    </body>
 </html>
