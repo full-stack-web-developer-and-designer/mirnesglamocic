e45106d672a5803998664e47067389449bcccdbd
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-12-04 08:51:09 +0100

    Changed variable names to lowercase and lowerCamelCase

--- cms.php
+++ cms.php
@@ -1,63 +1,53 @@
 <?php
-	require_once 'core/init.php';
-	$head = Head::get(5);
-    $page_id=5;
+    require_once 'core/init.php';
+    $head = Head::get(5);
+    $page_id = 5;
     $page = Pages::get(5);
 ?>
 <!DOCTYPE html>
 <html itemscope lang="en">
-	
-	<?php 
-		echo $head->render();
-	?>
-
-	<body>
-		<?php include 'inc/header.php';?>
-		<div class="wrapper">
-
-			<?php echo $page->render(); ?>
-			
-			<section class="slider-container" aria-label="WordPress certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$WordPress = new WordPressSlide();
-						$WordPress->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="WordPress certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$WordPressPlugin = new WordPressPluginSlide();
-						$WordPressPlugin->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="WordPress certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$WordPressTheme = new WordPressThemeSlide();
-						$WordPressTheme->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="WordPress certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$WordPressEcommerce = new WordPressEcommerceSlide();
-						$WordPressEcommerce->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-		</div><!--end .wrapper-->
-		<?php include_once './inc/footer.php';//end #footer?>
-	</body>
+    <?php echo $head->render(); ?>
+    <body>
+        <?php include 'inc/header.php'; ?>
+        <div class="wrapper">
+            <?php echo $page->render(); ?>
+            
+            <section class="slider-container" aria-label="WordPress certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $wordpress = new WordPressSlide();
+                        $wordpress->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="WordPress certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $wordpressPlugin = new WordPressPluginSlide();
+                        $wordpressPlugin->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="WordPress certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $wordpressTheme = new WordPressThemeSlide();
+                        $wordpressTheme->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="WordPress certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $wordpressEcommerce = new WordPressEcommerceSlide();
+                        $wordpressEcommerce->renderSlider();
+                    ?>
+                </ul>
+            </section>
+        </div>
+        <?php include_once './inc/footer.php';//end #footer ?>
+    </body>
 </html>
