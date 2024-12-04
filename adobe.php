fd402ac23a01f248ed92f43f67ed24d5510f2ec8
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-12-04 06:27:43 +0100

    Changed variable names to lowercase and lowerCamelCase

--- adobe.php
+++ adobe.php
@@ -1,51 +1,44 @@
 <?php
-	require_once 'core/init.php';
-	$head = Head::get(10);
-    $page_id=10;
+    require_once 'core/init.php';
+    $head = Head::get(10);
+    $page_id = 10;
     $page = Pages::get(10);
 ?>
 <!DOCTYPE html>
 <html itemscope lang="en">
-	<?php
-		echo $head->render();
-	?>
-	<body>
-		<?php include 'inc/header.php';?>
-		<div class="wrapper">
+    <?php echo $head->render(); ?>
+    <body>
+        <?php include 'inc/header.php'; ?>
+        <div class="wrapper">
+            <?php echo $page->render(); ?>
+            
+            <section class="slider-container" aria-label="Photoshop CC certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $photoshop = new PhotoshopSlide();
+                        $photoshop->renderSlider();
+                    ?>
+                </ul>
+            </section>
+            
+            <section class="slider-container" aria-label="Photoshop CC certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $photoshopDesign = new PhotoshopDesignSlide();
+                        $photoshopDesign->renderSlider();
+                    ?>
+                </ul>
+            </section>
 
-			<?php echo $page->render(); ?>
-			
-			<section class="slider-container" aria-label="Photoshop CC certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$Photoshop = new PhotoshopSlide();
-						$Photoshop->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="Photoshop CC certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$PhotoshopDesign = new PhotoshopDesignSlide();
-						$PhotoshopDesign->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-			<section class="slider-container" aria-label="Lightroom CC certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
-				<ul class="content-slider cs-hidden">
-				<!-- slider box -->
-					<?php
-						$Lightroom = new LightroomSlide();
-						$Lightroom->renderSlider();
-					?>
-				</ul>
-			</section><!--end .slider-container-->
-
-		</div><!--end .wrapper-->
-		<?php include_once './inc/footer.php'; //end #footer ?>
-	</body>
+            <section class="slider-container" aria-label="Lightroom CC certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina">
+                <ul class="content-slider cs-hidden">
+                    <?php
+                        $lightroom = new LightroomSlide();
+                        $lightroom->renderSlider();
+                    ?>
+                </ul>
+            </section>
+        </div>
+        <?php include_once './inc/footer.php'; //end #footer ?>
+    </body>
 </html>
