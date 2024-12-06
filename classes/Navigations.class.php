69d7a0d39af8cd39f2946a9bb3b66b161b58ab79
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2024-12-06 06:10:25 +0100

    Changed order of navigation

--- classes/Navigations.class.php
+++ classes/Navigations.class.php
@@ -12,7 +12,7 @@ class Navigations extends ConnectSlider
             $title = strtoupper($row["title"]);
             $aria_label = ucfirst($row["title"]);
 
-            echo "<li><a href=\"/$href\" target=\"/$target\" title=\"View $aria_label\" 
+            echo "<li><a href=\"/$href\" target=\"$target\" title=\"View $aria_label\" 
 		aria-label=\"View $aria_label\">
 		$title</a>
 		<li>";
