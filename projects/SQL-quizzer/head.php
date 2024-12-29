c8df59869cced6ae046e5a62390410f5bd588c0a
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2024-12-29 11:03:11 +0100

    Create jpg and webp images for social app

--- projects/SQL-quizzer/head.php
+++ projects/SQL-quizzer/head.php
@@ -6,6 +6,36 @@
     <title>SQL Quizzer</title>
     <meta name="description" content="SQL quiz to test your knowledge with some questions from W3Schools">
     <meta name="author" content="Mirnes Glamočić">
+    <!-- Facebook & Viber -->
+    <meta name="title" property="og:title" content="SQL Quizzer">
+
+    <!-- Facebook, Viber, LinkedIn, Whatsapp & KakaoTalk(meta tags) -->
+    <meta property="og:url" content="https://mirnesglamocic.com/sql-quizzer/index.html">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://mirnesglamocic.com/projects/SQL-quizzer/SQLquizzer-og.jpg">
+    <meta property="og:image:type" content="image/jpg">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="og:image:alt" content="SQL Quizzer">
+    <meta property="og:site_name" content="SQL Quizzer">
+    <meta property="og:locale" content="en_us">
+    <meta property="fb:app_id" content="1568922700258839">
+
+    <!-- LinkedIn, Whatsapp & KakaoTalk(meta tag) -->
+    <meta property="og:title" content="SQL Quizzer">
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="SQL Quizzer">
+    <meta name="twitter:description" content="SQL quiz to test your knowledge with some questions from W3Schools">
+    <meta name="twitter:image:src" content="https://mirnesglamocic.com/projects/SQL-quizzer/SQLquizzer-og.webp">
+    <meta name="twitter:image:alt" content="SQL Quizzer">
+
+    <!-- Google / Search Engine Tags -->
+    <meta itemprop="name" content="https://mirnesglamocic.com/sql-quizzer/index.html">
+    <meta itemprop="description" content="SQL quiz to test your knowledge with some questions from W3Schools">
+    <meta itemprop="image" content="SQL Quizzer">
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="../projects/SQL-quizzer/style.min.css">
      <!-- include google fonts -->
     <link rel="dns-prefetch" href="//fonts.googleapis.com">
