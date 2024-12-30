1f33a933981bb7123961cdfbaddd1aa069434019
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2024-12-30 04:48:09 +0100

    Create favicon for SQL-quizzer

--- projects/SQL-quizzer/head.php
+++ projects/SQL-quizzer/head.php
@@ -43,4 +43,8 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i&display=swap&amp;subset=latin-ext" rel="stylesheet">
     <!-- include fontawesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <link rel="apple-touch-icon" sizes="180x180" href="../projects/SQL-quizzer/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../projects/SQL-quizzer/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../projects/SQL-quizzer/favicon-16x16.png">
+    <link rel="manifest" href="../projects/SQL-quizzer/site.webmanifest">
 </head>
