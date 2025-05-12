a7b599209a15fc4ba24bec59e9c286112f322f27
Mirnes Glamočić <contact@mirnesglamocic.com>
2025-05-13 00:31:57 +0200

    Created new class for GIT certificates

--- classes/GitNewSlide.class.php
+++ classes/GitNewSlide.class.php
@@ -15,7 +15,6 @@ class GitNewSlide extends Slider
             echo "<li class='item-a slide'>
             <article class='cert' aria-label='GIT certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina'>
             <a href='./cert/BIG/$picture.webp' data-lightbox='image-group'><img src='./cert/SMALL/$picture.webp' alt=\"$alt by Web Designer and Developer Mirnes Glamočić from Bosnia and Herzegovina\" class=\"$class\" loading=\"lazy\"></a>
-            <a href='./cert/BIG/$picture.webp' data-lightbox='image-group'><img src='./cert/SMALL/$picture.webp' alt=\"$alt by Web Designer and Developer Mirnes Glamočić from Bosnia and Herzegovina\" class=\"$class\" loading=\"lazy\"></a>
             </article></li>";
         }
     }
