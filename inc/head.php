771ae12544aa7dcf5e42570c985dc2580dd692ca
Mirnes Glamočić <contact@mirnesglamocic.com>
2025-11-25 14:35:24 +0100

    Removed http-eqiv because a document must not include both a \“meta\” element with an \“http-equiv\” attribute whose value is \“content-type\”, and a \“meta\” element with a \“charset\” attribute.

--- inc/head.php
+++ inc/head.php
@@ -1,9 +1,8 @@
 <head>
+<meta charset="UTF-8">
 <!--
 Wow! Welcome to my code. If you need a professional website don't hesitate to contact me: https://mirnesglamocic.com
 -->
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php echo $title; ?></title>
 <meta name="description" content="<?php echo $meta['description']; ?>">
