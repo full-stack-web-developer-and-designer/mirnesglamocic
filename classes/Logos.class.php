6ee848d3694264010af5e48eec9e2730e95987d2
Mirnes Glamočić <contact@mirnesglamocic.com>
2025-12-29 13:41:12 +0100

    Changed name of column article to category in MySQL table

--- classes/Logos.class.php
+++ classes/Logos.class.php
@@ -5,16 +5,16 @@ class Logos extends ConnectSlider
     public function renderSlider()
     {
         $sql =
-            "SELECT * FROM mirnesgl_korea.projects WHERE article='logo' ORDER BY projects_id DESC;";
+            "SELECT * FROM mirnesgl_korea.projects WHERE category='logo' ORDER BY projects_id DESC;";
         $stmt = $this->__connect()->query($sql);
 
         while ($row = $stmt->fetch()) {
-            $article = $row["article"];
+            $category = $row["category"];
             $image = $row["image"];
             $alt = $row["alt"];
             $content = $row["content"];
 
-            echo "<article class=\"$article\"  aria-label='Projects of logos by professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina'><a href=\"./logos/BIG/$image.jpg\" data-lightbox='logos'><img src=\"./logos/SMALL/$image.jpg\" alt=\"$alt by professional web designer and programmer Mirnes Glamočić from Bosnia and Herzegovina\" class='blur' loading='lazy'><div class='content fade'><p>$content</p></div></a></article>";
+            echo "<article class=\"$category\"  aria-label='Projects of logos by professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina'><a href=\"./logos/BIG/$image.jpg\" data-lightbox='logos'><img src=\"./logos/SMALL/$image.jpg\" alt=\"$alt by professional web designer and programmer Mirnes Glamočić from Bosnia and Herzegovina\" class='blur' loading='lazy'><div class='content fade'><p>$content</p></div></a></article>";
         }
     }
 }
