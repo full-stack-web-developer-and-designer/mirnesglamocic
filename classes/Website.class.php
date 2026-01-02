4dec375044ee9a1bf3bef7b87afc9e932db87aa6
Mirnes Glamočić <contact@mirnesglamocic.com>
2026-01-02 23:32:10 +0100

    Improved SQL query with OR statement to show all projects

--- classes/Website.class.php
+++ classes/Website.class.php
@@ -5,7 +5,7 @@ class Website extends ConnectSlider
     public function renderSlider()
     {
         $sql =
-            "SELECT * FROM mirnesgl_korea.projects_website WHERE category='featured' OR category='fullstack'  OR category='mini' OR category='wordpress' ORDER BY projects_website_id DESC;";
+            "SELECT * FROM mirnesgl_korea.projects_website WHERE category='featured' OR category='featured fullstack' OR category='featured mini' OR category='featured wordpress' OR category='fullstack'  OR category='mini' OR category='mini fullstack' OR  category='wordpress' ORDER BY projects_website_id DESC;";
         $stmt = $this->__connect()->query($sql);
 
         while ($row = $stmt->fetch()) {
