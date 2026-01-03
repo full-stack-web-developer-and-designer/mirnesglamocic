01cb2829172f99807e93d8ae41d21e46d326f312
Mirnes Glamočić <contact@mirnesglamocic.com>
2026-01-03 07:40:11 +0100

    Renamed PHP function renderSlider to renderSkills and removed comment for picture for IE 9

--- classes/Skills.class.php
+++ classes/Skills.class.php
@@ -2,10 +2,11 @@
 class Skills extends ConnectSlider
 {
     // Fetch data from MySQL using PDO - PHP Data Object
-    public function renderSlider()
+    public function renderSkills()
     {
         $sql = "SELECT * FROM mirnesgl_korea.skills;";
-        $stmt = $this->__connect()->query($sql);
+        $stmt = $this->__connect()->prepare($sql);
+        $stmt->execute();
 
         while ($row = $stmt->fetch()) {
             $img_120 = $row["img_120"];
@@ -15,7 +16,7 @@ class Skills extends ConnectSlider
             $img = $row["img"];
             $alt = $row["alt"];
 
-            echo "<picture class='skill'><!--[if IE 9]><video style='display: none;'><![endif]-->
+            echo "<picture class='skill'>
             <source srcset=\"./images/$img_100\" type=\"image/webp\" width=\"100\" height=\"100\" media=\"(min-width: 1153px)\">
             <source srcset=\"./images/$img_120\" type=\"image/webp\" width=\"120\" height=\"120\" media=\"(min-width: 1020px)\">
             <source srcset=\"./images/$img_110\" type=\"image/webp\" width=\"110\" height=\"110\" media=\"(min-width: 859px)\">
@@ -24,7 +25,6 @@ class Skills extends ConnectSlider
             <source srcset=\"./images/$img_110\" type=\"image/webp\" width=\"110\" height=\"110\" media=\"(min-width: 679px)\">
             <source srcset=\"./images/$img_105\" type=\"image/webp\" width=\"105\" height=\"105\" media=\"(min-width: 653px)\">
             <source srcset=\"./images/$img_100\" type=\"image/webp\" width=\"100\" height=\"100\" media=\"(max-width: 652px)\">
-            <!--[if IE 9]></video><![endif]-->
             <img decoding=\"async\" src=\"./images/$img\" width=\"165\" height=\"165\" loading=\"lazy\" alt='$alt' class='skill'></picture>";
         }
     }
