622e878ac8b2a27a4d804244f4c6922c1c42b53d
Mirnes Glamočić <contact@mirnesglamocic.com>
2026-01-06 07:35:12 +0100

    Added htmlspecialchars to PHP variables

--- classes/ITA.class.php
+++ classes/ITA.class.php
@@ -9,24 +9,25 @@ class ITA extends ConnectSlider
         $stmt->execute();
 
 
-        while ($row = $stmt->fetch()) {
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $id  = preg_replace('/[^a-z0-9_-]/i', '', $row['id']);
             $href = htmlspecialchars($row['href'], ENT_QUOTES, 'UTF-8');
-            $img_400 = $row["img_400"];
-            $img_300 = $row["img_300"];
-            $img_250 = $row["img_250"];
-            $img_200 = $row["img_200"];
-            $img_180 = $row["img_180"];
-            $img_170 = $row["img_170"];
-            $img = $row["img"];
+            $img_400 = htmlspecialchars($row["img_400"], ENT_QUOTES, 'UTF-8');
+            $img_300 = htmlspecialchars($row["img_300"], ENT_QUOTES, 'UTF-8');
+            $img_250 = htmlspecialchars($row["img_250"], ENT_QUOTES, 'UTF-8');
+            $img_200 = htmlspecialchars($row["img_200"], ENT_QUOTES, 'UTF-8');
+            $img_180 = htmlspecialchars($row["img_180"], ENT_QUOTES, 'UTF-8');
+            $img_170 = htmlspecialchars($row["img_170"], ENT_QUOTES, 'UTF-8');
+            $img     = htmlspecialchars($row["img"], ENT_QUOTES, 'UTF-8');
             $alt = htmlspecialchars($row['alt'], ENT_QUOTES, 'UTF-8');
 
             echo "<article id=\"$id\" class=\"cert-item\">
                     <a href=\"./images/BIG/$href\" data-lightbox=\"ita\">
                         <picture class='ita'>
-                            <source srcset=\"$img_400\" type=\"image/webp\" width=\"400\" height=\"565\" media=\"(min-width: 1261px)\">
-                            <source srcset=\"$img_300\" type=\"image/webp\" width=\"300\" height=\"424\" media=\"(min-width: 1024px)\">
-                            <source srcset=\"$img_250\" type=\"image/webp\" width=\"250\" height=\"353\" media=\"(min-width: 768px)\">
+                            <source srcset=\"$img_400\" type=\"image/webp\" media=\"(min-width: 962px)\">
+                            <!--<source srcset=\"$img_300\" type=\"image/webp\" width=\"300\" height=\"424\" media=\"(min-width: 1024px)\">-->
+                            <source srcset=\"$img_300\" type=\"image/webp\" media=\"(min-width: 832px)\">
+                            <source srcset=\"$img_250\" type=\"image/webp\" width=\"250\" height=\"353\" media=\"(min-width: 621px)\">
                             <source srcset=\"$img_200\" type=\"image/webp\" width=\"200\" height=\"283\" media=\"(min-width: 521px)\">
                             <source srcset=\"$img_180\" type=\"image/webp\" width=\"180\" height=\"254\" media=\"(min-width: 430px)\">
                             <source srcset=\"$img_170\" type=\"image/webp\" width=\"170\" height=\"240\" media=\"(min-width: 389px)\">
