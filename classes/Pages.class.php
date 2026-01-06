14a59c160ef33d6239bd2a8e5e2918e132ffbc30
Mirnes Glamočić <contact@mirnesglamocic.com>
2026-01-06 06:03:02 +0100

    Renamed function render to renderPage

--- classes/Pages.class.php
+++ classes/Pages.class.php
@@ -4,7 +4,7 @@ class Pages extends Entity
     public static $tableName = "pages";
     public static $keyColumn = "page_id";
 
-    public function render()
+    public function renderPage()
     {
         $render = "<h1>$this->title</h1>";
         return $render;
