ed47ca5560f0dfb38925d1c5330e867bbf9a5e3d
Mirnes Glamočić <contact@mirnesglamocic.com>
2026-01-06 06:11:03 +0100

    Renamed functions render (renderHead, renderPage) and renderSlider (renderCertificates)

--- docker.php
+++ docker.php
@@ -1,21 +1,20 @@
 <?php
     require_once 'core/init.php';
     $head = Head::get(12);
-    $page_id=12;
     $page = Pages::get(12);
 ?>
 <!DOCTYPE html>
 <html itemscope lang="en">
-	<?php echo $head->render(); ?>
+	<?php echo $head->renderHead(); ?>
 	<body>
 		<?php include 'inc/header.php'; ?>
 		<div class="wrapper">
-            <?php echo $page->render(); ?>
-            <section aria-label="Docker certificates of professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina"class="slider-container">
+            <?php echo $page->renderPage(); ?>
+            <section aria-label="Docker certificates" class="slider-container">
                 <ul class="content-slider cs-hidden">
                     <?php
                         $docker=new DockerSlide();
-                        $docker->renderSlider();
+                        $docker->renderCertificates();
                     ?>
                 </ul>
             </section>
