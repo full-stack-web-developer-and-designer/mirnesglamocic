43220b723f49fed5dc05107eea11b41ebf3a414b
Mirnes Glamočić <contact@mirnesglamocic.com>
2026-01-07 03:21:55 +0100

    Removed .class.php at the end of the file

---
+++ classes/Config.php
@@ -0,0 +1,18 @@
+<?php
+class Config
+{
+    public static function get($path = null)
+    {
+        if ($path) {
+            $result = $GLOBALS["config"];
+            $path = explode("/", $path);
+            foreach ($path as $part) {
+                if (isset($result[$part])) {
+                    $result = $result[$part];
+                }
+            }
+            return $result;
+        }
+        return false;
+    }
+}
