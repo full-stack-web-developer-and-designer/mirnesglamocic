412cf744f928b52886034eee43bf1a02f2a9ca33
Mirnes Glamočić <contact@mirnesglamocic.com>
2026-01-12 00:10:12 +0100

    refactor(core): unify database connections using Entity base class

--- classes/ITA.php
+++ classes/ITA.php
@@ -1,41 +1,54 @@
 <?php
-class ITA extends ConnectSlider
+
+class ITA extends Entity
 {
-    // Fetch data from MySQL using PDO - PHP Data Object
-    public function renderCertificates()
+    protected static string $tableName = 'ita_certificates';
+    protected static string $keyColumn = 'id';
+
+    // DB columns (untyped on purpose)
+    public $id;
+    public $href;
+    public $img_400;
+    public $img_300;
+    public $img_250;
+    public $img_200;
+    public $img_180;
+    public $img_170;
+    public $img;
+    public $alt;
+
+    public function renderCertificates(): void
     {
-        $sql = "SELECT * FROM mirnesgl_korea.ita_certificates;";
-        $stmt = $this->__connect()->prepare($sql);
-        $stmt->execute();
+        foreach (self::getAll() as $row) {
 
+            $id   = preg_replace('/[^a-z0-9_-]/i', '', (string) $row->id);
+            $href = htmlspecialchars($row->href, ENT_QUOTES, 'UTF-8');
 
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $id  = preg_replace('/[^a-z0-9_-]/i', '', $row['id']);
-            $href = htmlspecialchars($row['href'], ENT_QUOTES, 'UTF-8');
-            $img_400 = htmlspecialchars($row["img_400"], ENT_QUOTES, 'UTF-8');
-            $img_300 = htmlspecialchars($row["img_300"], ENT_QUOTES, 'UTF-8');
-            $img_250 = htmlspecialchars($row["img_250"], ENT_QUOTES, 'UTF-8');
-            $img_200 = htmlspecialchars($row["img_200"], ENT_QUOTES, 'UTF-8');
-            $img_180 = htmlspecialchars($row["img_180"], ENT_QUOTES, 'UTF-8');
-            $img_170 = htmlspecialchars($row["img_170"], ENT_QUOTES, 'UTF-8');
-            $img     = htmlspecialchars($row["img"], ENT_QUOTES, 'UTF-8');
-            $alt = htmlspecialchars($row['alt'], ENT_QUOTES, 'UTF-8');
+            $img_400 = htmlspecialchars($row->img_400, ENT_QUOTES, 'UTF-8');
+            $img_300 = htmlspecialchars($row->img_300, ENT_QUOTES, 'UTF-8');
+            $img_250 = htmlspecialchars($row->img_250, ENT_QUOTES, 'UTF-8');
+            $img_200 = htmlspecialchars($row->img_200, ENT_QUOTES, 'UTF-8');
+            $img_180 = htmlspecialchars($row->img_180, ENT_QUOTES, 'UTF-8');
+            $img_170 = htmlspecialchars($row->img_170, ENT_QUOTES, 'UTF-8');
+            $img     = htmlspecialchars($row->img, ENT_QUOTES, 'UTF-8');
+            $alt     = htmlspecialchars($row->alt, ENT_QUOTES, 'UTF-8');
 
-            echo "<article id=\"$id\" class=\"cert-item\">
-                    <a href=\"./images/BIG/$href\" data-lightbox=\"ita\">
-                        <picture class='ita'>
-                            <source srcset=\"$img_400\" type=\"image/webp\" media=\"(min-width: 962px)\">
-                            <!--<source srcset=\"$img_300\" type=\"image/webp\" width=\"300\" height=\"424\" media=\"(min-width: 1024px)\">-->
-                            <source srcset=\"$img_300\" type=\"image/webp\" media=\"(min-width: 832px)\">
-                            <source srcset=\"$img_250\" type=\"image/webp\" width=\"250\" height=\"353\" media=\"(min-width: 621px)\">
-                            <source srcset=\"$img_200\" type=\"image/webp\" width=\"200\" height=\"283\" media=\"(min-width: 521px)\">
-                            <source srcset=\"$img_180\" type=\"image/webp\" width=\"180\" height=\"254\" media=\"(min-width: 430px)\">
-                            <source srcset=\"$img_170\" type=\"image/webp\" width=\"170\" height=\"240\" media=\"(min-width: 389px)\">
-                            <source srcset=\"$img_300\" type=\"image/webp\" width=\"300\" height=\"424\" media=\"(max-width: 388px)\">
-
-                            <img decoding=\"async\" src=\"$img\" width=\"400\" height=\"565\" loading=\"lazy\" alt=\"$alt\">
-                        </picture></a>
-                </article>";
+            echo <<<HTML
+<article id="$id" class="cert-item">
+    <a href="./images/BIG/$href" data-lightbox="ita">
+        <picture class="ita">
+            <source srcset="$img_400" type="image/webp" media="(min-width: 962px)">
+            <source srcset="$img_300" type="image/webp" media="(min-width: 832px)">
+            <source srcset="$img_250" type="image/webp" media="(min-width: 621px)">
+            <source srcset="$img_200" type="image/webp" media="(min-width: 521px)">
+            <source srcset="$img_180" type="image/webp" media="(min-width: 430px)">
+            <source srcset="$img_170" type="image/webp" media="(min-width: 389px)">
+            <source srcset="$img_300" type="image/webp" media="(max-width: 388px)">
+            <img decoding="async" src="$img" width="400" height="565" loading="lazy" alt="$alt">
+        </picture>
+    </a>
+</article>
+HTML;
         }
     }
 }
