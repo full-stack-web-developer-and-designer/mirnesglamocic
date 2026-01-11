c7b578853b62987eb3dedda9a4daabf107bfb56f
Mirnes Glamočić <contact@mirnesglamocic.com>
2026-01-11 23:11:51 +0100

    refactor: replace navigation slider with menu

--- inc/header.php
+++ inc/header.php
@@ -43,7 +43,10 @@
             <nav id="nav"><a href="#" id="respmenu"><svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 0 448 512"><path fill="#FFF" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg><h2>MENU</h2></a>
                 <ul class="menu">
                     <!-- navigation -->
-                    <?php $Navigations=new Navigations();$Navigations->renderSlider();?>
+                    <?php
+                        $Navigation = new Navigation(); 
+                        $Navigation->renderMenu();
+                    ?>
                 </ul>
             </nav>
         </div><!--end .wrapper-->
