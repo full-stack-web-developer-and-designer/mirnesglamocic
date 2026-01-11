1c94af4f865c4c3803b2dbafe373c61a51b29eda
Mirnes Glamočić <contact@mirnesglamocic.com>
2026-01-12 00:40:12 +0100

    Add Logos projects

---
+++ classes/Logos.php
@@ -0,0 +1,20 @@
+<?php
+class Logos extends ConnectSlider
+{
+    // Fetch data from MySQL using PDO - PHP Data Object
+    public function renderSlider()
+    {
+        $sql =
+            "SELECT * FROM mirnesgl_korea.projects WHERE category='logo' ORDER BY projects_id DESC;";
+        $stmt = $this->__connect()->query($sql);
+
+        while ($row = $stmt->fetch()) {
+            $category = $row["category"];
+            $image = $row["image"];
+            $alt = $row["alt"];
+            $content = $row["content"];
+
+            echo "<article class=\"$category\"  aria-label='Projects of logos by professional web developer and web designer Mirnes Glamočić from Bosnia and Herzegovina'><a href=\"./logos/BIG/$image.jpg\" data-lightbox='logos'><img src=\"./logos/SMALL/$image.jpg\" alt=\"$alt by professional web designer and programmer Mirnes Glamočić from Bosnia and Herzegovina\" class='blur' loading='lazy'><div class='content fade'><p>$content</p></div></a></article>";
+        }
+    }
+}
