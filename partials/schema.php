0ed5644d573511c5305c59eeb1fcb6941f0aadb0
Mirnes Glamočić <contact@mirnesglamocic.com>
2026-02-11 15:55:34 +0100

    chore: add partials/schema.php for project structure

---
+++ partials/schema.php
@@ -0,0 +1,125 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@graph": [
+
+    {
+      "@type": "Person",
+      "@id": "https://mirnesglamocic.com/#person",
+      "name": "Mirnes Glamočić",
+      "url": "https://mirnesglamocic.com",
+      "jobTitle": [
+        "Professional Web Developer",
+        "Professional Web Designer"
+      ],
+      "telephone": "+38763734327",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Borovi 22",
+        "postalCode": "70101",
+        "addressLocality": "Jajce",
+        "addressCountry": "BA"
+      },
+      "knowsAbout": [
+        "Web Development",
+        "Web Design",
+        "Frontend Development",
+        "Backend Development",
+        "Full Stack Development",
+        "SEO",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "PHP",
+        "MySQL",
+        "Accessibility (WCAG)"
+      ],
+      "sameAs": [
+        "https://github.com/full-stack-web-developer-and-designer",
+        "https://www.linkedin.com/in/mirnesglamocic"
+      ]
+    },
+
+    {
+      "@type": "ProfessionalService",
+      "@id": "https://mirnesglamocic.com/#business",
+      "name": "Mirnes Glamočić – Web Developer & Web Designer",
+      "url": "https://mirnesglamocic.com",
+      "image": "https://mirnesglamocic.com/images/Mirnes_og.jpg",
+      "description": "Professional web development and web design services in Jajce, Bosnia and Herzegovina.",
+      "provider": {
+        "@id": "https://mirnesglamocic.com/#person"
+      },
+      "telephone": "+38763734327",
+      "contactPoint": {
+        "@type": "ContactPoint",
+        "telephone": "+38763734327",
+        "contactType": "customer service",
+        "areaServed": "BA",
+        "availableLanguage": ["English"]
+      },
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Borovi 22",
+        "postalCode": "70101",
+        "addressLocality": "Jajce",
+        "addressCountry": "BA"
+      },
+      "areaServed": [
+        {
+          "@type": "Country",
+          "name": "Bosnia and Herzegovina"
+        },
+        {
+          "@type": "AdministrativeArea",
+          "name": "Europe"
+        }
+      ],
+      "hasOfferCatalog": {
+        "@type": "OfferCatalog",
+        "name": "Web Development Services",
+        "itemListElement": [
+          {
+            "@type": "Offer",
+            "itemOffered": {
+              "@type": "Service",
+              "name": "Website Development"
+            }
+          },
+          {
+            "@type": "Offer",
+            "itemOffered": {
+              "@type": "Service",
+              "name": "Web Design & UI/UX"
+            }
+          }
+        ]
+      }
+    },
+
+    {
+      "@type": "WebSite",
+      "@id": "https://mirnesglamocic.com/#website",
+      "url": "https://mirnesglamocic.com",
+      "name": "Mirnes Glamočić – Web Developer & Designer",
+      "publisher": {
+        "@id": "https://mirnesglamocic.com/#person"
+      },
+      "inLanguage": "en"
+    },
+
+    {
+      "@type": "WebPage",
+      "@id": "https://mirnesglamocic.com/#homepage",
+      "url": "https://mirnesglamocic.com",
+      "name": "Portfolio of Mirnes Glamočić",
+      "isPartOf": {
+        "@id": "https://mirnesglamocic.com/#website"
+      },
+      "about": {
+        "@id": "https://mirnesglamocic.com/#business"
+      }
+    }
+  ]
+}
+</script>
